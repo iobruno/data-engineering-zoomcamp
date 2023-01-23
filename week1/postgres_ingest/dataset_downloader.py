f875618b4eba0ec4745536d7b57fa5168f4a67e5
iobruno <bruno.ariev@gmail.com>
2023-01-23 00:52:17 -0300

    Add rich-lib into the ingestion scripts
    * Includes rich lib to enable better experience for the CLI
    
    * Adds up progress bars for input nyc-tlc-yellow-tripdata to postgres
    
    * DRY up on dataset_downloader.py

--- week1/postgres_ingest/dataset_downloader.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-
-def load_csv_from(url: str) -> pd.DataFrame:
-    return pd.read_csv(url, engine='pyarrow')
--- week1/postgres_ingest/main.py
+++ week1/postgres_ingest/main.py
@@ -1,16 +1,28 @@
 import os
+import pandas as pd
 
-from dataset_downloader import load_csv_from
-from df_persistence import persist_df
-
+from df_persistence import persist_df, persist_df_with, split_df_in_chunks_with
 from omegaconf import OmegaConf
 from pathlib import Path
 from sqlalchemy import create_engine
 from typing import List
-
+from rich.progress import (
+    BarColumn,
+    Progress,
+    TaskID,
+    TextColumn,
+)
 
 config_file = Path(__file__).parent.joinpath("app.yml")
 cfg = OmegaConf.load(config_file)
+
+progress = Progress(
+    TextColumn("[bold blue]{task.description}", justify="right"),
+    BarColumn(),
+    "Chunk: {task.completed}/{task.total}",
+    "•",
+    "[progress.percentage]{task.percentage:>3.1f}%"
+)
 
 
 def setup_db_conn():
@@ -25,14 +37,27 @@ def setup_db_conn():
 
 def ingest_yellow_taxi_data_with(db_conn):
     yellow_trip_data_urls: List[str] = cfg.datasets.yellow_trip_data
+    filenames = list(map(lambda string: string.split("/")[-1], yellow_trip_data_urls))
 
-    for url in yellow_trip_data_urls:
-        print(f"Now Fetching: '{url}'...")
-        df = load_csv_from(url=url)
-        persist_df(df=df, conn=db_conn, table_name="ntl_yellow_taxi")
+    with progress:
+        task_ids: List[TaskID] = [progress.add_task(name, start=False, total=0) for name in filenames]
+
+        for idx, url in enumerate(yellow_trip_data_urls):
+            df = pd.read_csv(url, engine='pyarrow')
+            dfs, qty = split_df_in_chunks_with(df)
+
+            progress.start_task(task_id=task_ids[idx])
+            progress.update(task_id=task_ids[idx], completed=0, total=qty)
+
+            for chunk_id, new_df in enumerate(dfs):
+                persist_df_with(df=new_df, conn=db_conn, table_name="ntl_yellow_taxi")
+                progress.update(task_id=task_ids[idx], completed=chunk_id+1)
+
+            progress.stop_task(task_id=task_ids[idx])
 
 
 if __name__ == "__main__":
     conn = setup_db_conn()
     conn.connect()
     ingest_yellow_taxi_data_with(db_conn=conn)
+    exit(0)
