234da4bcf805dfc3c386329cd77e1c16d0f5be13
iobruno <bruno.ariev@gmail.com>
2023-01-25 19:52:36 -0300

    Create gcs bucket for the 'raw' layer of the Lake
    - Include a gcs bucket named "iobruno_dtc_datalake_raw", meant for the
      Datalake on us-central1
    - Enfoces uniform bucket level access
    - Enforces cascade delete of all objects within

--- week1/terraform/main.tf
+++ week1/terraform/main.tf
@@ -1,13 +1,25 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
-      version = "4.50.0"
+      source  = "hashicorp/google"
+      version = "~> 4.50.0"
     }
   }
 }
 
 provider "google" {
-    project = "iobruno-data-eng-zoomcamp"
-    region = "us-central1-a"
+  project = "iobruno-data-eng-zoomcamp"
+  region  = "us-central1-a"
+}
+
+# Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket
+resource "google_storage_bucket" "gcs_datalake_raw_bucket" {
+  name     = "iobruno_dtc_datalake_raw"
+  location = "us-central1"
+
+  storage_class               = "STANDARD"
+  uniform_bucket_level_access = true
+
+  # Cascade delete all objects within when the bucket is deleted
+  force_destroy = true
 }
