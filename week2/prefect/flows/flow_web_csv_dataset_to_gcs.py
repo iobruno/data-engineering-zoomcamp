ebb0a75de38d9e27321c1df8aaae45c8d358ce22
iobruno <bruno.ariev@gmail.com>
2023-02-08 14:02:44 -0300

    Implementing CSV to GCS for Green Taxi data

--- week2/prefect/flows/flow_web_csv_dataset_to_gcs.py
+++ week2/prefect/flows/flow_web_csv_dataset_to_gcs.py
@@ -35,44 +35,44 @@ def load_into_gcs_with(bucket_name: str, blob_name: str, fs_path: str):
 
 
 @task(log_prints=True, retries=3)
-def save_as_parquet_to_fs_with(df: pd.DataFrame, label: str) -> Tuple[Path, str]:
+def save_to_fs_with(df: pd.DataFrame, label: str) -> Tuple[Path, str]:
     filename = f"{label.split('.')[0]}.parquet.gz"
     file_dir = root_dir.joinpath("datasets")
     file_dir.mkdir(parents=True, exist_ok=True)
     filepath = file_dir.joinpath(filename)
     df.to_parquet(filepath, compression='gzip')
+    print(f"Dataset '{label}' contains: {len(df)} lines")
     return filepath, filename
 
 
 @task(log_prints=True, retries=3)
-def fix_datatypes_for(df: pd.DataFrame) -> pd.DataFrame:
-    return df.astype({
-        'PUlocationID': 'Int64',
-        'DOlocationID': 'Int64',
-        'SR_Flag': 'Int64'
-    })
-
-
-@task(log_prints=True, retries=3)
-def fetch_fhv_csv_from(url: str) -> pd.DataFrame:
+def fetch_csv_from(url: str) -> pd.DataFrame:
     print(f"Now fetching: {url}")
     return pd.read_csv(url, engine='pyarrow')
 
 
-@flow(name="NYC FHV CSV Dataset to GCS", log_prints=True)
+@flow(name="NYC Taxi Trip data CSV Dataset to GCS", log_prints=True)
 def ingest():
     try:
         print("Fetching URL Datasets from .yml")
         datasets = cfg.datasets
 
-        if datasets.fhv:
-            for endpoint in datasets.fhv:
+        if datasets.green_trip_data:
+            for endpoint in datasets.green_trip_data:
                 filename = endpoint.split("/")[-1]
-                df = fetch_fhv_csv_from(url=endpoint)
-                cleansed_df = fix_datatypes_for(df=df)
-                filepath, parquet_filename = save_as_parquet_to_fs_with(df=cleansed_df, label=filename)
+                df = fetch_csv_from(url=endpoint)
+                filepath, parquet_filename = save_to_fs_with(df=df, label=filename)
                 load_into_gcs_with(bucket_name=cfg.gcp.gcs_target_bucket,
-                                   blob_name=f"fhv/{parquet_filename}",
+                                   blob_name=f"green/{parquet_filename}",
+                                   fs_path=filepath)
+
+        if datasets.yellow_trip_data:
+            for endpoint in datasets.yellow_trip_data:
+                filename = endpoint.split("/")[-1]
+                df = fetch_csv_from(url=endpoint)
+                filepath, parquet_filename = save_to_fs_with(df=df, label=filename)
+                load_into_gcs_with(bucket_name=cfg.gcp.gcs_target_bucket,
+                                   blob_name=f"yellow/{parquet_filename}",
                                    fs_path=filepath)
 
     except Exception as ex:
