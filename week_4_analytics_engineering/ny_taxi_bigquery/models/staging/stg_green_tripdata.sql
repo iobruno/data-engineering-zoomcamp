2e221c2e33dcf28031ebc47f7a3d127b2a230b4d
iobruno <bruno.ariev@gmail.com>
2023-02-25 14:04:50 -0300

    [dbt] Add dbt model for 'stg_yellow_tripdata' and 'stg_green_tripdata'
    * Add dbt model for green_tripdata and yellow_tripdata with
      'stg_green_tripdata.sql', and 'stg_yellow_tripdata.sql' respectively
    
    * Add dbt macro 'resolve_payment_type_description'
    
    * Include 'dbt_utils' on packages.yml to generate surrogate_keys
      for both models: 'green' and 'yellow'
    
    * Generate surrogate keys for green_tripdata with:
      'VendorID', 'lpep_pickup_datetime', 'PULocationID', 'DOLocationID',
    
    * Generate surrogate keys for yellow_tripdata with:
      'VendorID', 'tpep_pickup_datetime', 'PULocationID', 'DOLocationID'
    
    * Include VAR 'is_test_run' on both models that add that a LIMIT clause,
      for faster runs (meant for debugging purposes). When executed as:
      """ dbt run --select stg_green_tripdata --var 'is_test_run: true' """,
      the view will be built on top of a result set of 100 rows total
    
    * Drop models/example/*.sql

---
+++ week_4_analytics_engineering/ny_taxi_bigquery/models/staging/stg_green_tripdata.sql
@@ -0,0 +1,48 @@
+{{ config(materialized='view') }}
+
+
+SELECT 
+    -- identifiers
+    {{ dbt_utils.generate_surrogate_key(['VendorID', 'PULocationID', 'DOLocationID', 'lpep_pickup_datetime']) }} as trip_id,
+    VendorID as vendor_id,
+    RatecodeID as ratecode_id,
+    PULocationID as pickup_location_id,
+    DOLocationID as dropoff_location_id,
+
+    -- pickup and dropoff timestamps
+    lpep_pickup_datetime as pickup_datetime,
+    lpep_dropoff_datetime as dropoff_datetime,
+
+    -- trip info
+    store_and_fwd_flag,
+    passenger_count,
+    trip_distance,
+    trip_type,
+
+    -- payment info
+    cast(fare_amount as numeric) as fare_amount,
+    cast(extra as numeric) as extra,
+    cast(mta_tax as numeric) as mta_tax,
+    cast(tip_amount as numeric) as tip_amount,
+    cast(tolls_amount as numeric) as tolls_amount,
+    cast(ehail_fee as numeric) as ehail_fee,
+    cast(improvement_surcharge as numeric) as improvement_surcharge,
+    cast(congestion_surcharge as numeric) as congestion_surcharge,
+    cast(total_amount as numeric) as total_amount,
+    cast(payment_type as integer) as payment_type,
+    {{ resolve_payment_type_desc_for('payment_type') }} as payment_type_desc, 
+
+FROM 
+    {{ source('staging', 'green_tripdata') }}
+
+WHERE 
+    VendorID IS NOT NULL
+
+-- Run as:
+--  dbt build --select stg_green_tripdata --var 'is_test_run: true'
+--  dbt run --select stg_green_tripdata --var 'is_test_run: false'
+{% if var('is_test_run', default=true) %}
+
+    LIMIT 100
+
+{% endif %}
