dd44cb6db4f06078d8a77d48b864f606066c1c46
iobruno <bruno.ariev@gmail.com>
2023-03-01 23:55:37 -0300

    Modularizing PySpark Jupyter playground
    * Add 'app.py' as the application entrypoint
    
    * Add spark_gcs handle fetching csv from GCS

---
+++ week5/pyspark/app.py
@@ -0,0 +1,75 @@
+from pyspark.sql import SparkSession
+from spark_gcs import read_csv_from_gcs
+
+
+def config_spark_session(name: str, master: str) -> SparkSession:
+    spark = SparkSession.builder\
+        .config("spark.sql.execution.arrow.pyspark.enabled", "true")\
+        .config("spark.driver.memory", "2g")\
+        .config("spark.executor.memory", "8g")\
+        .config("spark.cores.max", 8)\
+        .appName(name)\
+        .master(master)\
+        .getOrCreate()
+
+    spark._jsc.hadoopConfiguration()\
+        .set("google.cloud.auth.service.account.json.keyfile", "")
+
+    return spark
+
+
+def join_dataframe_with_spark_sql(spark: SparkSession):
+    return spark.sql(""""
+        WITH t_fhv AS (
+            SELECT
+                dispatching_base_num, 
+                Affiliated_base_number as affiliated_base_num,
+                pickup_datetime,
+                dropOff_datetime as dropoff_datetime,
+                PUlocationID as pickup_location_id,
+                DOlocationID as dropoff_location_id
+            FROM fhv
+        ),
+        
+        t_zones AS (
+            SELECT
+                LocationID as location_id,
+                Borough as borough,
+                Zone as zone,
+                service_zone as service_zone                
+            FROM zones        
+        )    
+        
+        SELECT 
+            f.dispatching_base_num,
+            f.affiliated_base_num,
+    
+            -- Pickup Location
+            f.pickup_datetime,
+            pu.zone as pickup_zone,
+            pu.service_zone as pickup_service_zone,
+            
+            -- Dropoff Location
+            f.dropoff_location_id,
+            do.zone as dropoff_zone,
+            do.service_zone as dropoff_service_zone
+                    
+        FROM t_fhv f    
+        INNER JOIN t_zones pu ON f.pickup_location_id  = pu.location_id
+        INNER JOIN t_zones do ON f.dropoff_location_id = do.location_id
+    """)
+
+
+def main():
+    spark = config_spark_session(name="pyspark-playground",
+                                 master="local[*]")
+
+    read_csv_from_gcs(spark, gcs_prefix="", view_name="fhv")
+    read_csv_from_gcs(spark, gcs_prefix="", view_name="zones")
+
+    sdf = join_dataframe_with_spark_sql(spark)
+    sdf.printSchema()
+
+
+if __name__ == "__main__":
+    main()
---
+++ week5/pyspark/spark_gcs.py
@@ -0,0 +1,25 @@
+
+from pyspark.sql import DataFrame
+from pyspark.sql.session import SparkSession
+
+
+def read_csv_from_gcs(spark: SparkSession,
+                      gcs_prefix: str,
+                      view_name: str = None,
+                      schema: str = None,
+                      has_header: bool = True) -> DataFrame:
+    if schema:
+        sdf = spark.read\
+            .option("header", has_header)\
+            .option("inferSchema", True)\
+            .csv(path=gcs_prefix)
+    else:
+        sdf = spark.read\
+            .option("header", has_header)\
+            .option("inferSchema", False)\
+            .csv(path=gcs_prefix)
+
+    if view_name:
+        sdf.createTempView(view_name)
+
+    return sdf
