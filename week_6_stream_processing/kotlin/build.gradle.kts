01115b9c2083abd7561b9481009d84af06d99287
iobruno <bruno.ariev@gmail.com>
2023-03-04 00:10:18 -0300

    [Stream-Processing] Bootstrap stream-processing with Kotlin
    * Add dependencies to handle interactions connections
      with Kafka and Kafka Streams
    
    * Add dependencies for serialization and deserialization (serde)
      with Protobuf, Avro and JSON
    
    * Add 'kotlin-logging-jvm' library as a slf4j wrapper for Kotlin,
      'logback-classic' as an logging implementation for slf4j
    
    * Add 'logback.xml' with configs for sync and async logging
    
    * Add frameworks to handle Unit Tests (jUnit 5), mocks (MockK),
      and assertions (Kotest) in Kotlin-idiomatic way
    
    * Add shadowJar gradle plugin to gen uber/fat jars
    
    * Add rides.csv in src/main/resources
    
    * Add .editorconfig & README.md

---
+++ week_6_stream_processing/kotlin/build.gradle.kts
@@ -0,0 +1,83 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+plugins {
+    kotlin("jvm") version "1.8.10"
+    id("com.github.johnrengelman.shadow") version "8.1.0"
+}
+
+val artifactName = "kafka-stream-processing"
+val artifactVersion = "1.0-SNAPSHOT"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven {
+        name = "Confluent Maven Repository"
+        url = uri("https://packages.confluent.io/maven/")
+    }
+}
+
+dependencies {
+    val confluentKafkaVersion = "7.3.2"
+    val avroVersion = "1.11.1"
+    val protobufVersion = "3.22.0"
+    val openCsvVersion = "5.7.1"
+
+    val kotlinLoggingVersion = "3.0.5"
+    val logbackVersion = "1.4.5"
+
+    val junitVersion = "5.9.2"
+    val mockKVersion = "1.13.4"
+    val kotestVersion = "5.5.5"
+
+    implementation("org.apache.kafka:kafka-clients:${confluentKafkaVersion}-ccs")
+    implementation("org.apache.kafka:kafka-streams:${confluentKafkaVersion}-ccs")
+    implementation("io.confluent:kafka-schema-registry-client:${confluentKafkaVersion}")
+
+    /** Kafka and Kafka Streams Serde for Avro **/
+    implementation("io.confluent:kafka-avro-serializer:${confluentKafkaVersion}")
+    implementation("io.confluent:kafka-streams-avro-serde:${confluentKafkaVersion}")
+    implementation("org.apache.avro:avro:${avroVersion}")
+
+    /** Kafka and Kafka Streams Serde for Protobuf **/
+    implementation("io.confluent:kafka-protobuf-serializer:${confluentKafkaVersion}")
+    implementation("io.confluent:kafka-streams-protobuf-serde:${confluentKafkaVersion}")
+    implementation("com.google.protobuf:protobuf-java:$protobufVersion")
+
+    /** Kafka and Kafka Streams Serde for JSON Schema **/
+    implementation("io.confluent:kafka-json-serializer:${confluentKafkaVersion}")
+    implementation("io.confluent:kafka-streams-json-schema-serde:${confluentKafkaVersion}")
+
+    /** CSV Parser **/
+    implementation("com.opencsv:opencsv:${openCsvVersion}")
+
+    /** Logging **/
+    implementation("io.github.microutils:kotlin-logging-jvm:${kotlinLoggingVersion}")
+    implementation("ch.qos.logback:logback-classic:${logbackVersion}")
+
+    /** Unit and Integration Test Frameworks **/
+    testImplementation("org.apache.kafka:kafka-streams-test-utils:${confluentKafkaVersion}-ccs")
+    testImplementation("org.junit.jupiter:junit-jupiter:$junitVersion")
+    testImplementation("io.mockk:mockk:$mockKVersion")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
+}
+
+tasks.withType<ShadowJar> {
+    archiveBaseName.set("${artifactName}-${artifactVersion}")
+    mergeServiceFiles()
+    manifest {
+        attributes(mapOf("Main-Class" to "club.datatalks.kafka.ApplicationKt"))
+    }
+    isZip64 = true
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
