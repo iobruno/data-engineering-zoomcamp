5301a8f31d8d60a45ae47cfc1570ccac3e562a30
iobruno <bruno.ariev@gmail.com>
2023-03-08 23:59:20 -0300

    [Stream Processing] Add DTOs and CsvDeserializable
    * Add CsvDeserializable Interface with seqFromCsv()
    
    * Add dataclasses for FhvTaxiDTO, GreenTaxiDTO, YellowTaxiDTO
    
    * Implement CsvDeserializable on all DTOs
    
    * Implement KafkaSerializable interface on all DTOs
    
    * Include jackson-module-kotlin, and jackson-datatype-jsr310
      to support Java 8 DateTimes

---
+++ week_6_stream_processing/kotlin/src/main/kotlin/club/datatalks/kafka/dto/FhvTaxiDTO.kt
@@ -0,0 +1,42 @@
+package club.datatalks.kafka.dto
+
+import club.datatalks.kafka.infrastructure.CsvDeserializable
+import club.datatalks.kafka.infrastructure.KafkaSerializable
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+import com.fasterxml.jackson.dataformat.csv.CsvSchema
+import java.io.BufferedReader
+
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
+data class FhvTaxiDTO(
+    val dispatchingBaseNumber: String,
+    val pickupDatetime: String,
+    val dropoffDatetime: String,
+    val pickupLocationId: Int?,
+    val dropoffLocationId: Int?,
+    val srFlag: String?,
+    val affiliatedBaseNumber: String
+) : KafkaSerializable {
+
+    companion object {
+
+        fun fromCsv(reader: BufferedReader, containsHeader: Boolean = true): Sequence<FhvTaxiDTO> =
+            CsvDeserializable.seqFromCsv(reader, schema = csvSchema(), containsHeader = containsHeader)
+
+        private fun csvSchema(): CsvSchema =
+            CsvSchema.builder()
+                .addColumn("dispatching_base_number")
+                .addColumn("pickup_datetime")
+                .addColumn("dropoff_datetime")
+                .addColumn("pickup_location_id")
+                .addColumn("dropoff_location_id")
+                .addColumn("sr_flag")
+                .addColumn("affiliated_base_number")
+                .build()
+    }
+
+    override fun messageKey(): String = pickupLocationId.toString()
+
+
+}
