906186308f4178a13ff70acaeca5933085886492
iobruno <bruno.ariev@gmail.com>
2023-03-09 11:53:32 -0300

    Implement messageKey() on RideDTO
    * Rename Ride -> RideDTO
    
    * Implement messageKey using pickupLocationId

--- week_6_stream_processing/kotlin/src/main/kotlin/club/datatalks/kafka/dto/RideDTO.kt
+++ week_6_stream_processing/kotlin/src/main/kotlin/club/datatalks/kafka/dto/RideDTO.kt
@@ -1,12 +1,13 @@
 package club.datatalks.kafka.dto
 
+import club.datatalks.kafka.infrastructure.KafkaSerializable
 import com.fasterxml.jackson.dataformat.csv.CsvMapper
 import com.fasterxml.jackson.dataformat.csv.CsvSchema
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import java.io.BufferedReader
 
-data class Ride(
+data class RideDTO(
     val vendorId: Int,
     val pickupDateTime: String,
     val dropOffDateTime: String,
@@ -25,10 +26,10 @@ data class Ride(
     val improvementSurcharge: Double,
     val totalAmount: Double,
     val congestionSurcharge: Double
-) {
+) : KafkaSerializable {
 
     companion object {
-        fun listFromCsv(reader: BufferedReader, containsHeader: Boolean = true): List<Ride> {
+        fun listFromCsv(reader: BufferedReader, containsHeader: Boolean = true): List<RideDTO> {
             val schema = if (containsHeader)
                 csvSchema().withHeader()
             else
@@ -39,31 +40,34 @@ data class Ride(
                 .registerModule(JavaTimeModule())
 
             return mapper
-                .readerFor(Ride::class.java)
+                .readerFor(RideDTO::class.java)
                 .with(schema)
-                .readValues<Ride>(reader)
+                .readValues<RideDTO>(reader)
                 .readAll()!!
         }
 
         private fun csvSchema(): CsvSchema = CsvSchema.builder()
-            .addColumn(Ride::vendorId.name)
-            .addColumn(Ride::pickupDateTime.name)
-            .addColumn(Ride::dropOffDateTime.name)
-            .addColumn(Ride::passengerCount.name)
-            .addColumn(Ride::tripDistance.name)
-            .addColumn(Ride::rateCodeId.name)
-            .addColumn(Ride::storeAndForward.name)
-            .addColumn(Ride::pickupLocationId.name)
-            .addColumn(Ride::dropOffLocationId.name)
-            .addColumn(Ride::paymentType.name)
-            .addColumn(Ride::fareAmount.name)
-            .addColumn(Ride::extra.name)
-            .addColumn(Ride::mtaTax.name)
-            .addColumn(Ride::tipAmount.name)
-            .addColumn(Ride::tollsAMount.name)
-            .addColumn(Ride::improvementSurcharge.name)
-            .addColumn(Ride::totalAmount.name)
-            .addColumn(Ride::congestionSurcharge.name)
+            .addColumn(RideDTO::vendorId.name)
+            .addColumn(RideDTO::pickupDateTime.name)
+            .addColumn(RideDTO::dropOffDateTime.name)
+            .addColumn(RideDTO::passengerCount.name)
+            .addColumn(RideDTO::tripDistance.name)
+            .addColumn(RideDTO::rateCodeId.name)
+            .addColumn(RideDTO::storeAndForward.name)
+            .addColumn(RideDTO::pickupLocationId.name)
+            .addColumn(RideDTO::dropOffLocationId.name)
+            .addColumn(RideDTO::paymentType.name)
+            .addColumn(RideDTO::fareAmount.name)
+            .addColumn(RideDTO::extra.name)
+            .addColumn(RideDTO::mtaTax.name)
+            .addColumn(RideDTO::tipAmount.name)
+            .addColumn(RideDTO::tollsAMount.name)
+            .addColumn(RideDTO::improvementSurcharge.name)
+            .addColumn(RideDTO::totalAmount.name)
+            .addColumn(RideDTO::congestionSurcharge.name)
             .build()
     }
+
+    override fun messageKey(): String = pickupLocationId.toString()
+
 }
