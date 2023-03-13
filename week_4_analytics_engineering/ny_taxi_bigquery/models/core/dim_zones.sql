aa7914d813b498174ccd14d00d10e00d72bccbcf
iobruno <bruno.ariev@gmail.com>
2023-03-12 22:52:43 -0300

    [PipeRider] Add fact tables and dimension zone_lookup
    * Add fact_yellow_green_tripdata, including:
      * 'service_type' to differentiate green from yellow,
      * 'row_num' partitioned by vendor_id and pickup_datetime for the dedup strategy
      *  eliminate rows where vendor_id is null
    
    * Build dim_zone_lookup straight from stg_zone_lookup.sql

--- week_4_analytics_engineering/ny_taxi_bigquery/models/core/dim_zones.sql
+++
@@ -1,9 +0,0 @@
-{{ config(materialize='table') }}
-
-SELECT
-    LocationID as location_id,
-    Borough as borough,
-    Zone as zone,
-    REPLACE(service_zone, 'Boro', 'Green') as service_zone
-FROM     
-    {{ ref('taxi_zone_lookup') }}
