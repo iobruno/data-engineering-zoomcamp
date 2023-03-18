037f9ec682fe2493028dc71ce8109a6ebfa2fbb2
iobruno <bruno.ariev@gmail.com>
2023-03-17 23:18:41 -0300

    Add PySpark to read_csv for Kafka
    * Bootstrap 'main.py' & add datasets/rides.csv
    
    * Add dependencies for: faust-streaming, pyspark,
      avro and protobuf on pyproject.toml

---
+++ week_6_stream_processing/python/main.py
@@ -0,0 +1,27 @@
+from pyspark.sql import SparkSession
+from pathlib import Path
+
+root_dir = Path(__file__).parent
+dataset_dir = root_dir.joinpath("datasets")
+
+
+def config_spark_session(name: str, master: str = "local[*]") -> SparkSession:
+    spark = SparkSession.builder \
+            .config("spark.sql.execution.arrow.pyspark.enabled", "true") \
+            .config("spark.driver.memory", "2g") \
+            .config("spark.executor.memory", "8g") \
+            .config("spark.cores.max", 8) \
+            .appName(name) \
+            .master(master) \
+            .getOrCreate()
+
+    return spark
+
+
+if __name__ == "__main__":
+    spark = config_spark_session(name="pyspark-streaming")
+    rides_df = spark.read\
+        .option("header", True)\
+        .csv(path=str(dataset_dir))
+
+    rides_df.printSchema()
