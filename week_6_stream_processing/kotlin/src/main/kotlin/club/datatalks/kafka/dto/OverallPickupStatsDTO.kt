91f74c73a9e10a2e55a8a59625362ac540436092
iobruno <bruno.ariev@gmail.com>
2023-03-26 19:46:03 -0300

    [Stream Processing] Implement Consumers for ksqlDB KTable
    * Implement OverallPickupStatsConsumer for ksqlDB KTable,
      and add a new DTO model for it: OverallPickupStatsDTO

---
+++ week_6_stream_processing/kotlin/src/main/kotlin/club/datatalks/kafka/dto/OverallPickupStatsDTO.kt
@@ -0,0 +1,18 @@
+package club.datatalks.kafka.dto
+
+import club.datatalks.kafka.infrastructure.KafkaSerializable
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+
+@JsonNaming(PropertyNamingStrategies.UpperSnakeCaseStrategy::class)
+data class OverallPickupStatsDTO(
+    val dummyCol: Int,
+    val totalGreenRecords: Long,
+    val totalFhvRecords: Long,
+    val overallRecords: Long
+) : KafkaSerializable {
+
+    override fun messageKey(): String? = null
+
+}
