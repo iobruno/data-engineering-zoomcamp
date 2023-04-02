46d196ddf328d388fc682dfdeb1d2579651c2eb0
iobruno <bruno.ariev@gmail.com>
2023-04-02 11:26:57 -0300

    Add Kafka Producers and Consumers
    * Add KafkaJsonProducerService with 'fromCsv()' implementation
    
    * Add KafkaJsonConsumerService with 'start()' implementation
    
    * Add TaxiProducer & TaxiConsumer
    
    * Add KotlinLogger to AbstractKafkaJsonProducer,
      and AbstractKafkaJsonConsumer
    
    * Fix on YellowTaxiConsumer which was deserializing
      to a non-KafkaSerializable class

---
+++ week_6_stream_processing/kotlin/src/main/kotlin/club/datatalks/kafka/TaxiConsumer.kt
@@ -0,0 +1,65 @@
+package club.datatalks.kafka
+
+import club.datatalks.kafka.dto.FhvTaxiDTO
+import club.datatalks.kafka.dto.GreenTaxiDTO
+import club.datatalks.kafka.dto.OverallPickupStatsDTO
+import club.datatalks.kafka.dto.YellowTaxiDTO
+
+class GreenTaxiConsumer(
+    private val kafkaTopic: String,
+    private val consumerGroup: String? = null,
+) : AbstractKafkaJsonConsumer<GreenTaxiDTO>(
+    kafkaTopic, consumerGroup, GreenTaxiDTO::class.java
+)
+
+class YellowTaxiConsumer(
+    private val kafkaTopic: String,
+    private val consumerGroup: String? = null,
+) : AbstractKafkaJsonConsumer<YellowTaxiDTO>(
+    kafkaTopic, consumerGroup, YellowTaxiDTO::class.java
+)
+
+class FhvTaxiConsumer(
+    private val kafkaTopic: String,
+    private val consumerGroup: String? = null,
+) : AbstractKafkaJsonConsumer<FhvTaxiDTO>(
+    kafkaTopic, consumerGroup, FhvTaxiDTO::class.java
+)
+
+class OverallPickupStatsConsumer(
+    private val kafkaTopic: String,
+    private val consumerGroup: String? = null,
+) : AbstractKafkaJsonConsumer<OverallPickupStatsDTO>(
+    kafkaTopic, consumerGroup, OverallPickupStatsDTO::class.java
+)
+
+
+fun main() {
+    /** Green Taxi Consumer **/
+    val greenTaxiConsumer = GreenTaxiConsumer(
+        kafkaTopic = "green_tripdata",
+        consumerGroup = "default"
+    )
+    greenTaxiConsumer.start()
+
+    /** Yellow Taxi Producer **/
+//    val yellowTaxiConsumer = YellowTaxiConsumer(
+//        kafkaTopic = "yellow_tripdata",
+//        consumerGroup = "default"
+//    )
+//    yellowTaxiConsumer.start()
+
+    /** Fhv Taxi Producer **/
+//    val fhvTaxiConsumer = FhvTaxiConsumer(
+//        kafkaTopic = "green_tripdata",
+//        consumerGroup = "default"
+//    )
+//    fhvTaxiConsumer.start()
+
+    /** Overall Pickup Aggregation Stats Consumer **/
+//    val overallPickupAggConsumer = OverallPickupStatsConsumer(
+//        kafkaTopic = "overall_pickup_agg",
+//        consumerGroup = "default"
+//    )
+//    overallPickupAggConsumer.start()
+}
---
+++ week_6_stream_processing/kotlin/src/main/kotlin/club/datatalks/kafka/service/KafkaJsonConsumerService.kt
@@ -0,0 +1,40 @@
+package club.datatalks.kafka.service
+
+import club.datatalks.kafka.infrastructure.KafkaJsonConsumer
+import club.datatalks.kafka.infrastructure.KafkaSerializable
+import mu.KotlinLogging
+import java.time.Duration
+
+class KafkaJsonConsumerService<T>(
+    private val topic: String,
+    private val consumerGroup: String? = null,
+    private val deserializationClass: Class<T>
+) where T: KafkaSerializable {
+
+    private val logger = KotlinLogging.logger {}
+
+    fun start() {
+        logger.info { "Starting Kafka Consumer binding on Topic='${topic}'..." }
+        val kafkaJsonConsumer: KafkaJsonConsumer<T> = KafkaJsonConsumer(deserializationClass)
+
+        while (true) {
+            val records = kafkaJsonConsumer.subscribeTo(
+                topic = topic,
+                pollingDuration = Duration.ofSeconds(5L),
+                consumerGroup = consumerGroup
+            )
+            if (!records.isEmpty) {
+                logger.info { "Fetching ${records.count()} records from from topic='${topic}'" }
+                records.map {
+                    val message: T = it.value()
+                    println(message)
+                }
+                logger.info { "Committing messages" }
+                kafkaJsonConsumer.commit()
+            } else {
+                logger.info { "Still polling..." }
+            }
+        }
+    }
+
+}
