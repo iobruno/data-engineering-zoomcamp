d0af87f813f19550c167263d2baa42a8e559b36b
iobruno <bruno.ariev@gmail.com>
2023-04-02 12:28:48 -0300

    [Stream Processing] Add companion object fun create() for Csv Deserialization
    * Add fun create() on FhvTaxiDTO, GreenTaxiDTO, and YellowTaxiDTO
      to enable deserialization from CsvFile using Jackson's @JsonCreator

--- week_6_stream_processing/kotlin/src/main/kotlin/club/datatalks/kafka/dto/FhvTaxiDTO.kt
+++ week_6_stream_processing/kotlin/src/main/kotlin/club/datatalks/kafka/dto/FhvTaxiDTO.kt
@@ -2,6 +2,8 @@ package club.datatalks.kafka.dto
 
 import club.datatalks.kafka.infrastructure.CsvDeserializable
 import club.datatalks.kafka.infrastructure.KafkaSerializable
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 import com.fasterxml.jackson.dataformat.csv.CsvSchema
@@ -20,6 +22,25 @@ data class FhvTaxiDTO(
 ) : KafkaSerializable {
 
     companion object {
+
+        /**
+         * create() is meant for each row from the CsvFile into an FhvTaxiDTO object
+         * using the Jackson's @JsonCreator
+         */
+        @JvmStatic
+        @JsonCreator
+        fun create(
+            @JsonProperty("dispatching_base_number") dispatchingBaseNumber: String,
+            @JsonProperty("pickup_datetime") pickupDatetime: String,
+            @JsonProperty("dropoff_datetime") dropoffDatetime: String,
+            @JsonProperty("pickup_location_id") pickupLocationId: Int?,
+            @JsonProperty("dropoff_location_id") dropoffLocationId: Int?,
+            @JsonProperty("sr_flag") srFlag: String?,
+            @JsonProperty("affiliated_base_number") affiliatedBaseNumber: String
+        ) = FhvTaxiDTO(
+            dispatchingBaseNumber, pickupDatetime, dropoffDatetime, pickupLocationId,
+            dropoffLocationId, srFlag, affiliatedBaseNumber
+        )
 
         fun fromCsv(reader: BufferedReader, containsHeader: Boolean = true): Sequence<FhvTaxiDTO> =
             CsvDeserializable.seqFromCsv(reader, schema = csvSchema(), containsHeader = containsHeader)
--- week_6_stream_processing/kotlin/src/main/kotlin/club/datatalks/kafka/dto/YellowTaxiDTO.kt
+++ week_6_stream_processing/kotlin/src/main/kotlin/club/datatalks/kafka/dto/YellowTaxiDTO.kt
@@ -2,6 +2,8 @@ package club.datatalks.kafka.dto
 
 import club.datatalks.kafka.infrastructure.CsvDeserializable
 import club.datatalks.kafka.infrastructure.KafkaSerializable
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 import com.fasterxml.jackson.dataformat.csv.CsvSchema
@@ -31,6 +33,38 @@ data class YellowTaxiDTO(
 ) : KafkaSerializable {
 
     companion object {
+
+        /**
+         * create() is meant for each row from the CsvFile into an YellowTaxiDTO object
+         * using the Jackson's @JsonCreator
+         */
+        @JvmStatic
+        @JsonCreator
+        fun create(
+            @JsonProperty("vendor_id") vendorId: Int,
+            @JsonProperty("pickup_datetime")  pickupDatetime: String,
+            @JsonProperty("dropoff_datetime") dropoffDatetime: String,
+            @JsonProperty("passenger_count") passengerCount: Int,
+            @JsonProperty("trip_distance") tripDistance: Double,
+            @JsonProperty("rate_code_id") rateCodeId: Int,
+            @JsonProperty("store_and_forward") storeAndForward: String,
+            @JsonProperty("pickup_location_id") pickupLocationId: Int,
+            @JsonProperty("dropoff_location_id") dropoffLocationId: Int,
+            @JsonProperty("payment_type") paymentType: Int,
+            @JsonProperty("fare_amount") fareAmount: Double,
+            @JsonProperty("extra") extra: Double,
+            @JsonProperty("mta_tax") mtaTax: Double,
+            @JsonProperty("tip_amount") tipAmount: Double,
+            @JsonProperty("tolls_amount") tollsAMount: Double,
+            @JsonProperty("improvement_surcharge") improvementSurcharge: Double,
+            @JsonProperty("total_amount") totalAmount: Double,
+            @JsonProperty("congestion_surcharge") congestionSurcharge: Double
+        ) = YellowTaxiDTO(
+            vendorId, pickupDatetime, dropoffDatetime, passengerCount, tripDistance, rateCodeId, storeAndForward,
+            pickupLocationId, dropoffLocationId, paymentType, fareAmount, extra, mtaTax, tipAmount, tollsAMount,
+            improvementSurcharge, totalAmount, congestionSurcharge
+        )
+
         fun fromCsv(reader: BufferedReader, containsHeader: Boolean = true): Sequence<YellowTaxiDTO> =
             CsvDeserializable.seqFromCsv(reader, schema = csvSchema(), containsHeader = containsHeader)
 
