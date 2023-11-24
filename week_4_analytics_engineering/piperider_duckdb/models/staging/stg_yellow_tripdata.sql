620bad77e6d8fdad97e6e2c6c06a0749cfde12da
Bruno Oliveira <bruno.ariev@gmail.com>
2023-11-24 13:56:39 -0300

    Move dbt-duckdb-piperider project
    * Move from all 'models/core', 'models/staging' and 'macros/'
      from 'workshop-piperider' to 'piperider_duckdb'
    
    * Move packages.yml and 'dbt_project.yml' to the new folder structure as well

--- week_4_analytics_engineering/piperider_duckdb/models/staging/stg_yellow_tripdata.sql
+++ week_4_analytics_engineering/piperider_duckdb/models/staging/stg_yellow_tripdata.sql
@@ -1,4 +1,39 @@
-{{ config(materialized='table') }}
+{{ config(materialized='view') }}
 
-SELECT *
+SELECT
+    -- identifiers
+    {{
+        dbt_utils.generate_surrogate_key([
+            'VendorID',
+            'tpep_pickup_datetime'
+        ])
+    }}                                          AS trip_id,
+    VendorID                                    AS vendor_id,
+    RatecodeID                                  AS ratecode_id,
+    PULocationID                                AS pickup_location_id,
+    DOLocationID                                AS dropoff_location_id,
+
+    -- pickup and dropoff timestamps
+    tpep_pickup_datetime                        AS pickup_datetime,
+    tpep_dropoff_datetime                       AS dropoff_datetime,
+
+    -- trip info
+    store_and_fwd_flag,
+    passenger_count,
+    trip_distance,
+    1                                           AS trip_type, -- yellow cabs are always street-hail
+
+    -- payment info
+    fare_amount                                 AS fare_amount,
+    extra                                       AS extra,
+    mta_tax                                     AS mta_tax,
+    tip_amount                                  AS tip_amount,
+    tolls_amount                                AS tolls_amount,
+    0                                           AS ehail_fee, -- it does not apply for yellow cabs
+    improvement_surcharge                       AS improvement_surcharge,
+    congestion_surcharge                        AS congestion_surcharge,
+    total_amount                                AS total_amount,
+    payment_type                                AS payment_type,
+    {{ payment_type_desc_for('payment_type') }} AS payment_type_desc
+
 FROM {{ source('parquet', 'yellow') }}
