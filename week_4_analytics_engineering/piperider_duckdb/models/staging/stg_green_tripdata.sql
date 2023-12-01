2dc8bbe8ab9f53e815cd728d6aba2ad6fb5d3b34
Bruno Oliveira <bruno.ariev@gmail.com>
2023-12-01 04:40:47 -0300

    revamp/week-4: dbt-duckdb-piperider (#16)
    * PEP-517: Packaging and dependency management with PDM
    * Narrows down python version to >=3.10,<3.12
    * Update dbt_utils to 1.1.1
    * Drops the metrics from models/core/schema.yml
    * Updates dbt `stage` models to read directly from Google Cloud Storage
      * Add gcsfs dependency to allow [dbt-duckdb](https://github.com/duckdb/dbt-duckdb) to [read from GCS through fsspec](https://gcsfs.readthedocs.io/en/latest/api.html?highlight=GCSFileSystem#gcsfs.core.GCSFileSystem)
    * Renames dbt `core` models:
      * Renames 'fact_fhv_trips.sql' to 'dim_fhv_trips.sql'
      * Renames 'fact_yellow_green_trips.sql' to 'dim_yellow_green_trips.sql'
      * Renames 'dim_monthly_zone_revenue.sql' to 'fact_monthly_zone_revenue.sql'
      * Renames 'dim_monthly_zone_statistics' to 'fact_monthly_zone_stats.sql'
    * Updates README
      * Adds shields for python, dbt, duckdb, and Creative Commons license
      * Update running instructions on setting up dev env with PDM
      * Updates TODO list
    * Updates .gitignore adding entries for dbt and piperider

--- week_4_analytics_engineering/piperider_duckdb/models/staging/stg_green_tripdata.sql
+++ week_4_analytics_engineering/piperider_duckdb/models/staging/stg_green_tripdata.sql
@@ -1,4 +1,34 @@
-{{ config(materialized='table') }}
-
-SELECT *
-FROM {{ source('parquet', 'green') }}
+SELECT
+    -- identifiers
+    {{ 
+        dbt_utils.generate_surrogate_key([
+            'VendorID',
+            'lpep_pickup_datetime'
+        ])
+    }}                      as trip_id,
+    VendorID                as vendor_id,
+    RatecodeID              as ratecode_id,
+    PULocationID            as pickup_location_id,
+    DOLocationID            as dropoff_location_id,
+    -- pickup and dropoff timestamps
+    lpep_pickup_datetime    as pickup_datetime,
+    lpep_dropoff_datetime   as dropoff_datetime,
+    -- trip info
+    store_and_fwd_flag,
+    passenger_count,
+    trip_distance,
+    trip_type,
+    -- payment info
+    fare_amount             as fare_amount,
+    extra                   as extra,
+    mta_tax                 as mta_tax,
+    tip_amount              as tip_amount,
+    tolls_amount            as tolls_amount,
+    ehail_fee               as ehail_fee,
+    improvement_surcharge   as improvement_surcharge,
+    congestion_surcharge    as congestion_surcharge,
+    total_amount            as total_amount,
+    payment_type            as payment_type,
+    {{ payment_type_desc_for('payment_type') }} as payment_type_desc
+FROM 
+    {{ source('nyc_trip_record_data_parquet', 'green') }}
