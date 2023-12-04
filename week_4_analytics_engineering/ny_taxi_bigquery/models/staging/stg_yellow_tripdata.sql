0ad1f90ae31fb5c5f408eb525e57904e52626ce5
Bruno Oliveira <bruno.ariev@gmail.com>
2023-12-04 06:50:53 -0300

    revamp/week-4: dbt-bigquery (#15)
    * PEP-517: Packaging and dependency management with PDM
    * Narrows down python version to >=3.10,<3.12
    * Update dbt_utils to 1.1.1
    * Update dbt `core` models:
      * Renames 'fact_fhv_trips.sql' to 'dim_fhv_trips.sql'
      * Renames 'fact_yellow_green_trips.sql' to 'dim_yellow_green_trips.sql'
      * Renames 'dim_monthly_zone_revenue.sql' to 'fact_monthly_zone_revenue.sql'
      * All models/core are now saved on their own dataset `nyc_trip_record_data`, which differs from the one in staging (`stg_nyc_trip_record_data`)
    * Adds generate_schema_name
      * Override 'generate_schema_name' macro default behavior for custom schemas. Now custom schema name won't be appended to `target.schema`
    * Updates README
      * Adds shields for python, dbt, duckdb, and Creative Commons license
      * Update running instructions on setting up dev env with PDM
      * Updates TODO list with dbt Labs courses
      * Update running instructions to run dbt end-to-end

--- week_4_analytics_engineering/ny_taxi_bigquery/models/staging/stg_yellow_tripdata.sql
+++ week_4_analytics_engineering/ny_taxi_bigquery/models/staging/stg_yellow_tripdata.sql
@@ -1,58 +1,44 @@
-{{ config(materialized='view') }}
-
-
-
-WITH yellow_trip_data AS (
-    SELECT
-        *, 
-        ROW_NUMBER() OVER(PARTITION BY VendorID, tpep_pickup_datetime) as row_num
-    FROM
-        {{ source('staging', 'yellow_tripdata') }}
-)
-
-SELECT 
+SELECT
     -- identifiers
-    {{ dbt_utils.generate_surrogate_key(['VendorID', 'tpep_pickup_datetime']) }} as trip_id,
-    VendorID as vendor_id,
-    RatecodeID as ratecode_id,
-    PULocationID as pickup_location_id,
-    DOLocationID as dropoff_location_id,
-
+    {{
+        dbt_utils.generate_surrogate_key([
+            'VendorID', 
+            'tpep_pickup_datetime'
+        ])
+    }}                      as trip_id,
+    VendorID                as vendor_id,
+    RatecodeID              as ratecode_id,
+    PULocationID            as pickup_location_id,
+    DOLocationID            as dropoff_location_id,
     -- pickup and dropoff timestamps
-    tpep_pickup_datetime as pickup_datetime,
-    tpep_dropoff_datetime as dropoff_datetime,
-
+    tpep_pickup_datetime    as pickup_datetime,
+    tpep_dropoff_datetime   as dropoff_datetime,
     -- trip info
-    store_and_fwd_flag,
-    passenger_count,
-    trip_distance,
-    1 as trip_type, -- yellow cabs are always street-hail
-
+    store_and_fwd_flag      as store_and_fwd_flag,
+    passenger_count         as passenger_count,
+    trip_distance           as trip_distance,
+    1                       as trip_type, -- yellow cabs are always street-hail
     -- payment info
-    cast(fare_amount as numeric) as fare_amount,
-    cast(extra as numeric) as extra,
-    cast(mta_tax as numeric) as mta_tax,
-    cast(tip_amount as numeric) as tip_amount,
-    cast(tolls_amount as numeric) as tolls_amount,
-    cast(0 as numeric) as ehail_fee, -- it does not apply for yellow cabs
-    cast(improvement_surcharge as numeric) as improvement_surcharge,
-    cast(congestion_surcharge as numeric) as congestion_surcharge,
-    cast(total_amount as numeric) as total_amount,
-    cast(payment_type as integer) as payment_type,
-    {{ resolve_payment_type_desc_for('payment_type') }} as payment_type_desc, 
-
+    fare_amount             as fare_amount,
+    extra                   as extra,
+    mta_tax                 as mta_tax,
+    tip_amount              as tip_amount,
+    tolls_amount            as tolls_amount,
+    0                       as ehail_fee, -- it does not apply for yellow cabs
+    improvement_surcharge   as improvement_surcharge,
+    congestion_surcharge    as congestion_surcharge,
+    total_amount            as total_amount,
+    payment_type            as payment_type,
+    {{ 
+        payment_type_desc_for('payment_type')
+    }}                      as payment_type_desc
 FROM 
-    yellow_trip_data
+    {{ source('bq-staging-nyc-trip_record', 'yellow') }}
 
-WHERE 
-    VendorID IS NOT NULL
-    AND row_num = 1
 
 -- Run as:
 --  dbt build --select stg_green_tripdata --var 'is_test_run: true'
 --  dbt run --select stg_green_tripdata --var 'is_test_run: false'
-{% if var('is_test_run', default=true) %}
-
+{% if var('is_test_run', default=false) %}
     LIMIT 100
-
 {% endif %}
