f31fcbe60ea41e0fd1339650d84a5ab5fad10c9d
Bruno Oliveira <bruno.ariev@gmail.com>
2023-12-04 05:21:55 -0300

    Update bigquery dataset name
    * Update dataset from 'stg_nyc_trip_record_data'
      to 'raw_nyc_trip_record_data'

--- week_3_data_warehouse/sql/nyc_trip_record_data_create_ext_tables.sql
+++ week_3_data_warehouse/sql/nyc_trip_record_data_create_ext_tables.sql
@@ -1,26 +1,26 @@
 -- EXTERNAL TABLES FROM PARQUET
-CREATE OR REPLACE EXTERNAL TABLE `iobruno-gcp-labs.stg_nyc_trip_record_data.ext_green`
+CREATE OR REPLACE EXTERNAL TABLE `iobruno-gcp-labs.raw_nyc_trip_record_data.ext_green`
 OPTIONS (
     format = "PARQUET",
     uris = ["gs://iobruno-lakehouse-raw/nyc_trip_record_data/green/*.parquet"]
 
 );
 
-CREATE OR REPLACE EXTERNAL TABLE `iobruno-gcp-labs.stg_nyc_trip_record_data.ext_yellow`
+CREATE OR REPLACE EXTERNAL TABLE `iobruno-gcp-labs.raw_nyc_trip_record_data.ext_yellow`
 OPTIONS (
     format = "PARQUET",
     uris = ["gs://iobruno-lakehouse-raw/nyc_trip_record_data/yellow/*.parquet"]
 
 );
 
-CREATE OR REPLACE EXTERNAL TABLE `iobruno-gcp-labs.stg_nyc_trip_record_data.ext_fhv`
+CREATE OR REPLACE EXTERNAL TABLE `iobruno-gcp-labs.raw_nyc_trip_record_data.ext_fhv`
 OPTIONS (
     format = "PARQUET",
     uris = ["gs://iobruno-lakehouse-raw/nyc_trip_record_data/fhv/*.parquet"]
 
 );
 
-CREATE OR REPLACE EXTERNAL TABLE `iobruno-gcp-labs.stg_nyc_trip_record_data.ext_fhvhv`
+CREATE OR REPLACE EXTERNAL TABLE `iobruno-gcp-labs.raw_nyc_trip_record_data.ext_fhvhv`
 OPTIONS (
     format = "PARQUET",
     uris = ["gs://iobruno-lakehouse-raw/nyc_trip_record_data/fhvhv/*.parquet"]
@@ -29,7 +29,7 @@ OPTIONS (
 
 
 -- EXTERNAL TABLES FROM CSV
-CREATE OR REPLACE EXTERNAL TABLE `iobruno-gcp-labs.stg_nyc_trip_record_data.ext_zone_lookup`
+CREATE OR REPLACE EXTERNAL TABLE `iobruno-gcp-labs.raw_nyc_trip_record_data.ext_zone_lookup`
 OPTIONS (
     format = "CSV",
     uris = ["gs://iobruno-lakehouse-raw/nyc_trip_record_data/zone_lookup/*.csv.gz"]
