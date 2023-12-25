6871b265759c9825a949f89341278aa29ce9df6b
Bruno Oliveira <bruno.ariev@gmail.com>
2023-12-24 22:39:44 -0300

    Format dbt-bigquery SQL files

--- week_4_analytics_engineering/bigquery/models/core/dim_fhv_trips.sql
+++ week_4_analytics_engineering/bigquery/models/core/dim_fhv_trips.sql
@@ -1,9 +1,9 @@
 {{ config(
-    schema=env_var('DBT_BIGQUERY_DATASET'))
-}}
+    schema=env_var('DBT_BIGQUERY_DATASET')
+) }}
 
-WITH fhv_tripdata as (
-    SELECT
+with fhv_tripdata as (
+    select
         dispatching_base_num,
         affiliated_base_num,
         pickup_datetime,
@@ -11,35 +11,34 @@ WITH fhv_tripdata as (
         pickup_location_id,
         dropoff_location_id,
         shared_ride_flag,
-        row_number() OVER (PARTITION BY dispatching_base_num, pickup_datetime) as row_num
-    FROM
+        row_number() over (partition by dispatching_base_num, pickup_datetime) as row_num
+    from 
         {{ ref('stg_fhv_tripdata') }}
 ),
 
-lookup_zones AS (
-    SELECT * FROM {{ ref('dim_zone_lookup' )}} 
-    WHERE borough != 'Unknown'
+lookup_zones as (
+    select * from {{ ref('dim_zone_lookup' )}} where borough != 'Unknown'
 )
 
-SELECT
-    t.dispatching_base_num  as dispatching_base_num,
-    t.affiliated_base_num   as affiliated_base_num,
-    t.pickup_location_id    as pickup_location_id,
-    pickup.borough          as pickup_borough,
-    pickup.zone             as pickup_zone,
-    pickup.service_zone     as pickup_service_zone,
-    t.dropoff_location_id   as dropoff_location_id,
-    dropoff.borough         as dropoff_borough,
-    dropoff.zone            as dropoff_zone,
-    dropoff.service_zone    as dropoff_service_zone,
-    t.shared_ride_flag      as shared_ride_flag,
-    t.pickup_datetime       as pickup_datetime,
-    t.dropoff_datetime      as dropoff_datetime
-FROM 
+select
+    t.dispatching_base_num as dispatching_base_num,
+    t.affiliated_base_num  as affiliated_base_num,
+    t.pickup_location_id   as pickup_location_id,
+    pickup.borough         as pickup_borough,
+    pickup.zone            as pickup_zone,
+    pickup.service_zone    as pickup_service_zone,
+    t.dropoff_location_id  as dropoff_location_id,
+    dropoff.borough        as dropoff_borough,
+    dropoff.zone           as dropoff_zone,
+    dropoff.service_zone   as dropoff_service_zone,
+    t.shared_ride_flag     as shared_ride_flag,
+    t.pickup_datetime      as pickup_datetime,
+    t.dropoff_datetime     as dropoff_datetime
+from  
     fhv_tripdata t
-INNER JOIN lookup_zones pickup  
-    ON t.pickup_location_id  = pickup.location_id
-INNER JOIN lookup_zones dropoff 
-    ON t.dropoff_location_id = dropoff.location_id
-WHERE 
+inner join 
+    lookup_zones pickup  on t.pickup_location_id  = pickup.location_id
+inner join 
+    lookup_zones dropoff on t.dropoff_location_id = dropoff.location_id
+where 
     t.row_num = 1
--- week_4_analytics_engineering/bigquery/models/core/dim_zone_lookup.sql
+++ week_4_analytics_engineering/bigquery/models/core/dim_zone_lookup.sql
@@ -1,11 +1,10 @@
 {{ config(
-    schema=env_var('DBT_BIGQUERY_DATASET'))
-}}
+    schema=env_var('DBT_BIGQUERY_DATASET')
+) }}
 
-SELECT
-    LocationID      as location_id,
-    Borough         as borough,
-    Zone            as zone,
-    service_zone    as service_zone
-FROM
-    {{ ref('taxi_zone_lookup') }}
+select
+    LocationID   as location_id,
+    Borough      as borough,
+    Zone         as zone,
+    service_zone as service_zone
+from {{ ref('taxi_zone_lookup') }}
