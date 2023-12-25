ac4e6cca1d4042b456ebf0e7f62565c1c521e754
Bruno Oliveira <bruno.ariev@gmail.com>
2023-12-25 01:24:21 -0300

    Format dbt-clickhouse SQL files

--- week_4_analytics_engineering/clickhouse/models/core/dim_zone_lookup.sql
+++ week_4_analytics_engineering/clickhouse/models/core/dim_zone_lookup.sql
@@ -1,12 +1,12 @@
 {{ config(
     schema=env_var('DBT_CLICKHOUSE_SCHEMA'),
-    materialized='table')
-}}
+    materialized='table'
+) }}
 
-SELECT
-    LocationID      as location_id,
-    Borough         as borough,
-    Zone            as zone,
-    service_zone    as service_zone
-FROM
+select
+    LocationID   as location_id,
+    Borough      as borough,
+    Zone         as zone,
+    service_zone as service_zone
+from 
     {{ ref('taxi_zone_lookup') }}
