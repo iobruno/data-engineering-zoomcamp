aa2ab9a07d74bf65876b497c824a4cf89a6b0497
Bruno Oliveira <bruno.ariev@gmail.com>
2023-12-25 16:16:14 -0300

    dbt-redshift: Add new macro to resolve schema_name
    * Add macro to set the configuration.schema for the models based on target.name and model_type
    
    * Adds the optional env_var 'DBT_REDSHIFT_STAGING_SCHEMA',
       which when not set will default to 'stg_' ~ 'DBT_REDSHIFT_TARGET_SCHEMA'
    
    * Adds the optional env_var 'DBT_REDSHIFT_USE_DATA_CATALOG',
       which when not set will default t0 '0'
    
    * When `DBT_REDSHIFT_USE_DATA_CATALOG` is set to 1, the `DBT_REDSHIFT_STAGING_SCHEMA`
       env_var will, instead, default to `awsdatacatalog`, and the source_schema can be set either on
       `DBT_REDSHIFT_SOURCE_SCHEMA` or `DBT_REDSHIFT_SOURCE_GLUE_CATALOG_DB`
    
    * env_var 'DBT_REDSHIFT_SCHEMA' is now 'DBT_REDSHIFT_TARGET_SCHEMA'
    
    * dbt-macro 'payment_type_desc_for' is now called 'payment_desc_of'
    
    * Increase thread count to 4 on target 'prod'
    
    * Update Dockerfile and README accordingly

--- week_4_analytics_engineering/redshift/models/staging/stg_green_tripdata.sql
+++ week_4_analytics_engineering/redshift/models/staging/stg_green_tripdata.sql
@@ -1,6 +1,5 @@
 {{ config(
-    schema='stg_' ~ env_var('DBT_REDSHIFT_SCHEMA'),
-    materialized='table'
+    schema=resolve_schema_for('staging')
 ) }}
 
 select
@@ -8,33 +7,31 @@ select
     {{ dbt_utils.generate_surrogate_key([
         'VendorID',
         'lpep_pickup_datetime'
-    ]) }}                 as trip_id,
-    VendorID              as vendor_id,
-    RatecodeID            as ratecode_id,
-    PULocationID          as pickup_location_id,
-    DOLocationID          as dropoff_location_id,
+    ]) }}                                 as trip_id,
+    VendorID                              as vendor_id,
+    RatecodeID                            as ratecode_id,
+    PULocationID                          as pickup_location_id,
+    DOLocationID                          as dropoff_location_id,
     -- pickup and dropoff timestamps
-    lpep_pickup_datetime  as pickup_datetime,
-    lpep_dropoff_datetime as dropoff_datetime,
+    lpep_pickup_datetime                  as pickup_datetime,
+    lpep_dropoff_datetime                 as dropoff_datetime,
     -- trip info
-    store_and_fwd_flag    as store_and_fwd_flag,
-    passenger_count       as passenger_count,
-    trip_distance         as trip_distance,
-    trip_type             as trip_type,
+    store_and_fwd_flag                    as store_and_fwd_flag,
+    passenger_count                       as passenger_count,
+    trip_distance                         as trip_distance,
+    trip_type                             as trip_type,
     -- payment info
-    fare_amount           as fare_amount,
-    extra                 as extra,
-    mta_tax               as mta_tax,
-    tip_amount            as tip_amount,
-    tolls_amount          as tolls_amount,
-    ehail_fee             as ehail_fee,
-    improvement_surcharge as improvement_surcharge,
-    congestion_surcharge  as congestion_surcharge,
-    total_amount          as total_amount,
-    payment_type          as payment_type,
-    {{ 
-        payment_type_desc_for('payment_type')
-    }}                    as payment_type_desc
+    fare_amount                           as fare_amount,
+    extra                                 as extra,
+    mta_tax                               as mta_tax,
+    tip_amount                            as tip_amount,
+    tolls_amount                          as tolls_amount,
+    ehail_fee                             as ehail_fee,
+    improvement_surcharge                 as improvement_surcharge,
+    congestion_surcharge                  as congestion_surcharge,
+    total_amount                          as total_amount,
+    payment_type                          as payment_type,
+    {{ payment_desc_of('payment_type') }} as payment_type_desc
 from 
     {{ source('redshift-raw-nyc-trip_record', 'green') }}
 where
