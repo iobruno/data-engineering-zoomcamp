80516bf9f62feb23ee65aec6fdbebef5ea274cca
Bruno Oliveira <bruno.ariev@gmail.com>
2024-01-20 04:59:50 -0300

    Updates dataframe_repository
    * Refactors save() to delegate persistence to either
      __save_pandas_df or __save_polars_df
    
    * Implements __save_polars_df()
    
    * Moves implementation on save to __save_pandas_df()

--- module1-data-ingestion/python-ingest/src/dataframe_repository.py
+++ module1-data-ingestion/python-ingest/src/dataframe_repository.py
@@ -1,6 +1,7 @@
 from abc import ABCMeta, abstractmethod
-from typing import List, Optional
+from typing import List, Optional, Union
 import pandas as pd
+import polars as pl
 import sqlalchemy
 
 
@@ -9,7 +10,24 @@ class SQLRepository(metaclass=ABCMeta):
         self.conn_string = conn_string
         self.conn = sqlalchemy.create_engine(conn_string).connect()
 
-    def save(self, df: pd.DataFrame) -> Optional[int]:
+    def save(self, df: Union[pd.DataFrame, pl.DataFrame]) -> Optional[int]:
+        if isinstance(df, pl.DataFrame):
+            return self.save_polars_df(df)
+        elif isinstance(df, pd.DataFrame):
+            return self.save_pandas_df(df)
+
+        raise RuntimeError("Unsupported Dataframe type."
+                           "Supported types are pandas or polars Dataframes only")
+
+    def save_polars_df(self, df: pl.DataFrame, engine="sqlalchemy"):
+        return df.write_database(
+            table_name=self.tbl_name,
+            connection=self.conn_string,
+            if_table_exists="append",
+            engine=engine,
+        )
+
+    def save_pandas_df(self, df: pd.DataFrame):
         return df.to_sql(self.tbl_name, con=self.conn, if_exists="append", index=False)
 
     def save_all(self, chunks: List[pd.DataFrame]):
@@ -22,7 +40,7 @@ class SQLRepository(metaclass=ABCMeta):
         raise NotImplementedError()
 
     @classmethod
-    def with_config(cls, *db_settings) -> 'SQLRepository':
+    def with_config(cls, *db_settings) -> "SQLRepository":
         (db_dialect, db_host, db_port, db_name, db_username, db_password) = db_settings
         if db_dialect == "postgresql":
             conn_prefix = f"postgresql+psycopg"
@@ -33,7 +51,9 @@ class SQLRepository(metaclass=ABCMeta):
         else:
             raise Exception("Unsupported dialect. Supported options are ['postgresql', 'mysql']")
 
-        conn_string: str = f'{conn_prefix}://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}'
+        conn_string: str = (
+            f"{conn_prefix}://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
+        )
         return cls.__call__(conn_string=conn_string)
 
 
