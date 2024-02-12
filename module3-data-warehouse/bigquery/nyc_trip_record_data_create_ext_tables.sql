26607d8bcbafd6e5d19ef0ebc8936db857e69692
Bruno Oliveira <bruno.ariev@gmail.com>
2024-02-12 03:00:50 -0300

    Refactor table names and gcs path

--- module3-data-warehouse/bigquery/nyc_trip_record_data_create_ext_tables.sql
+++ module3-data-warehouse/bigquery/nyc_trip_record_data_create_ext_tables.sql
@@ -1,37 +1,31 @@
 -- EXTERNAL TABLES FROM PARQUET
-CREATE OR REPLACE EXTERNAL TABLE `iobruno-gcp-labs.raw_nyc_trip_record_data.ext_green`
+CREATE OR REPLACE EXTERNAL TABLE `iobruno-gcp-labs.raw_nyc_tlc.ext_green`
 OPTIONS (
     format = "PARQUET",
-    uris = ["gs://iobruno-lakehouse-raw/nyc_trip_record_data/green/*.parquet"]
-
+    uris = ["gs://iobruno-lakehouse-raw/nyc_tlc_dataset/green_taxi_data/*.parquet"]
 );
 
-CREATE OR REPLACE EXTERNAL TABLE `iobruno-gcp-labs.raw_nyc_trip_record_data.ext_yellow`
+CREATE OR REPLACE EXTERNAL TABLE `iobruno-gcp-labs.raw_nyc_tlc.ext_yellow`
 OPTIONS (
     format = "PARQUET",
-    uris = ["gs://iobruno-lakehouse-raw/nyc_trip_record_data/yellow/*.parquet"]
-
+    uris = ["gs://iobruno-lakehouse-raw/nyc_tlc_dataset/yellow_taxi_data/*.parquet"]
 );
 
-CREATE OR REPLACE EXTERNAL TABLE `iobruno-gcp-labs.raw_nyc_trip_record_data.ext_fhv`
+CREATE OR REPLACE EXTERNAL TABLE `iobruno-gcp-labs.raw_nyc_tlc.ext_fhv`
 OPTIONS (
     format = "PARQUET",
-    uris = ["gs://iobruno-lakehouse-raw/nyc_trip_record_data/fhv/*.parquet"]
-
+    uris = ["gs://iobruno-lakehouse-raw/nyc_tlc_dataset/fhv_taxi_data/*.parquet"]
 );
 
-CREATE OR REPLACE EXTERNAL TABLE `iobruno-gcp-labs.raw_nyc_trip_record_data.ext_fhvhv`
+CREATE OR REPLACE EXTERNAL TABLE `iobruno-gcp-labs.raw_nyc_tlc.ext_fhvhv`
 OPTIONS (
     format = "PARQUET",
-    uris = ["gs://iobruno-lakehouse-raw/nyc_trip_record_data/fhvhv/*.parquet"]
-
+    uris = ["gs://iobruno-lakehouse-raw/nyc_tlc_dataset/fhvhv_taxi_data/*.parquet"]
 );
-
 
 -- EXTERNAL TABLES FROM CSV
-CREATE OR REPLACE EXTERNAL TABLE `iobruno-gcp-labs.raw_nyc_trip_record_data.ext_zone_lookup`
+CREATE OR REPLACE EXTERNAL TABLE `iobruno-gcp-labs.raw_nyc_tlc.ext_zone_lookup`
 OPTIONS (
     format = "CSV",
     uris = ["gs://iobruno-lakehouse-raw/nyc_trip_record_data/zone_lookup/*.csv.gz"]
-
 );
--- module3-data-warehouse/bigquery/nyc_trip_record_data_create_tables.sql
+++ module3-data-warehouse/bigquery/nyc_trip_record_data_create_tables.sql
@@ -1,38 +1,38 @@
 -- PARTITIONED TABLES
-CREATE OR REPLACE TABLE `iobruno-gcp-labs.raw_nyc_trip_record_data.green`
+CREATE OR REPLACE TABLE `iobruno-gcp-labs.raw_nyc_tlc.green`
 PARTITION BY DATE(lpep_pickup_datetime)
 AS (
     SELECT *
-    FROM `iobruno-gcp-labs.raw_nyc_trip_record_data.ext_green`
+    FROM `iobruno-gcp-labs.raw_nyc_tlc.ext_green`
 );
 
 
-CREATE OR REPLACE TABLE `iobruno-gcp-labs.raw_nyc_trip_record_data.yellow`
+CREATE OR REPLACE TABLE `iobruno-gcp-labs.raw_nyc_tlc.yellow`
 PARTITION BY DATE(tpep_pickup_datetime)
 AS (
     SELECT *
-    FROM `iobruno-gcp-labs.raw_nyc_trip_record_data.ext_yellow`
+    FROM `iobruno-gcp-labs.raw_nyc_tlc.ext_yellow`
 );
 
 
-CREATE OR REPLACE TABLE `iobruno-gcp-labs.raw_nyc_trip_record_data.fhv`
+CREATE OR REPLACE TABLE `iobruno-gcp-labs.raw_nyc_tlc.fhv`
 PARTITION BY DATE(pickup_datetime)
 AS (
     SELECT *
-    FROM `iobruno-gcp-labs.raw_nyc_trip_record_data.ext_fhv`
+    FROM `iobruno-gcp-labs.raw_nyc_tlc.ext_fhv`
 );
 
 
 -- NON-PARTITIONED TABLES
-CREATE OR REPLACE TABLE `iobruno-gcp-labs.raw_nyc_trip_record_data.fhvhv`
+CREATE OR REPLACE TABLE `iobruno-gcp-labs.raw_nyc_tlc.fhvhv`
 AS (
     SELECT *
-    FROM `iobruno-gcp-labs.raw_nyc_trip_record_data.ext_fhvhv`
+    FROM `iobruno-gcp-labs.raw_nyc_tlc.ext_fhvhv`
 );
 
 
-CREATE OR REPLACE TABLE `iobruno-gcp-labs.raw_nyc_trip_record_data.zone_lookup`
+CREATE OR REPLACE TABLE `iobruno-gcp-labs.raw_nyc_tlc.zone_lookup`
 AS (
     SELECT *
-    FROM `iobruno-gcp-labs.raw_nyc_trip_record_data.ext_zone_lookup`
+    FROM `iobruno-gcp-labs.raw_nyc_tlc.ext_zone_lookup`
 );
