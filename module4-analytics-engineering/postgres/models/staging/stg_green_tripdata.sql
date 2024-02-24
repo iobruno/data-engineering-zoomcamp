44f48900ca85e655ea3f5623846e9cb74ea98f2d
Bruno Oliveira <bruno.ariev@gmail.com>
2024-02-24 16:24:45 -0300

    Refactor all staging models for dbt-postgres

--- module4-analytics-engineering/postgres/models/staging/stg_green_tripdata.sql
+++ module4-analytics-engineering/postgres/models/staging/stg_green_tripdata.sql
@@ -2,41 +2,50 @@
     schema=resolve_schema_for('staging')
 ) }}
 
+with green_taxi_trips as (
+    select
+        row_number() over(partition by vendor_id, lpep_pickup_datetime) as row_num,
+        gt.*
+    from
+        {{ source('raw_nyc_tlc_record_data', 'green_taxi_trips') }} gt
+    where
+        vendor_id is not null        
+)
+
 select
     -- identifiers
     {{ dbt_utils.generate_surrogate_key([
         'vendorid',
         'lpep_pickup_datetime'
-    ]) }}                                 as trip_id,
-    vendorid                              as vendor_id,
-    RatecodeID                            as ratecode_id,
-    PULocationID                          as pickup_location_id,
-    DOLocationID                          as dropoff_location_id,
+    ]) }}                                   as trip_id,
+    vendor_id                               as vendor_id,
+    ratecode_id                             as ratecode_id,
+    pu_location_id                          as pickup_location_id,
+    do_location_id                          as dropoff_location_id,
     -- pickup and dropoff timestamps
-    lpep_pickup_datetime                  as pickup_datetime,
-    lpep_dropoff_datetime                 as dropoff_datetime,
+    lpep_pickup_datetime                    as pickup_datetime,
+    lpep_dropoff_datetime                   as dropoff_datetime,
     -- trip info
-    store_and_fwd_flag                    as store_and_fwd_flag,
-    passenger_count                       as passenger_count,
-    trip_distance                         as trip_distance,
-    trip_type                             as trip_type,
+    store_and_fwd_flag                      as store_and_fwd_flag,
+    passenger_count                         as passenger_count,
+    trip_distance                           as trip_distance,
+    trip_type                               as trip_type,
     -- payment info
-    fare_amount                           as fare_amount,
-    extra                                 as extra,
-    mta_tax                               as mta_tax,
-    tip_amount                            as tip_amount,
-    tolls_amount                          as tolls_amount,
-    ehail_fee                             as ehail_fee,
-    improvement_surcharge                 as improvement_surcharge,
-    congestion_surcharge                  as congestion_surcharge,
-    total_amount                          as total_amount,
-    payment_type                          as payment_type,
-    {{ payment_desc_of('payment_type') }} as payment_type_desc
+    fare_amount                             as fare_amount,
+    extra                                   as extra,
+    mta_tax                                 as mta_tax,
+    tip_amount                              as tip_amount,
+    tolls_amount                            as tolls_amount,
+    ehail_fee                               as ehail_fee,
+    improvement_surcharge                   as improvement_surcharge,
+    congestion_surcharge                    as congestion_surcharge,
+    total_amount                            as total_amount,
+    payment_type                            as payment_type,
+    {{ payment_desc_of('payment_type') }}   as payment_type_desc
 from 
-    {{ source('postgres-raw-nyc-trip_record', 'ntl_green_taxi') }}
+    green_taxi_trips
 where
-    vendorid is not null
-
+    row_num = 1
 
 -- Run as:
 --  dbt build --select stg_green_tripdata --vars 'is_test_run: true'
