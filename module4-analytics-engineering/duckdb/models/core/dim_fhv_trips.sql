0459840e505565147266b0df3692d5b9c2a50417
Bruno Oliveira <bruno.ariev@gmail.com>
2024-02-24 19:14:14 -0300

    Refactor all core models

--- module4-analytics-engineering/duckdb/models/core/dim_fhv_trips.sql
+++ module4-analytics-engineering/duckdb/models/core/dim_fhv_trips.sql
@@ -1,41 +1,39 @@
-WITH fhv_tripdata as (
-    SELECT
+with fhv_trips as (
+    select
         dispatching_base_num,
         affiliated_base_num,
         pickup_datetime,
         dropoff_datetime,
         pickup_location_id,
         dropoff_location_id,
-        shared_ride_flag,
-        row_number() OVER (PARTITION BY dispatching_base_num, pickup_datetime) as row_num
-    FROM
+        shared_ride_flag
+    from
         {{ ref('stg_fhv_tripdata') }}
 ),
 
 lookup_zones AS (
-    SELECT * FROM {{ ref('dim_zone_lookup' )}} 
-    WHERE borough != 'Unknown'
+    select * 
+    from {{ ref('dim_zone_lookup' )}} 
+    where borough != 'Unknown'
 )
 
-SELECT
-    t.dispatching_base_num  as dispatching_base_num,
-    t.affiliated_base_num   as affiliated_base_num,
-    t.pickup_location_id    as pickup_location_id,
+select
+    ft.dispatching_base_num as dispatching_base_num,
+    ft.affiliated_base_num  as affiliated_base_num,
+    ft.pickup_location_id   as pickup_location_id,
     pickup.borough          as pickup_borough,
     pickup.zone             as pickup_zone,
     pickup.service_zone     as pickup_service_zone,
-    t.dropoff_location_id   as dropoff_location_id,
+    ft.dropoff_location_id  as dropoff_location_id,
     dropoff.borough         as dropoff_borough,
     dropoff.zone            as dropoff_zone,
     dropoff.service_zone    as dropoff_service_zone,
-    t.shared_ride_flag      as shared_ride_flag,
-    t.pickup_datetime       as pickup_datetime,
-    t.dropoff_datetime      as dropoff_datetime
-FROM 
-    fhv_tripdata t
-INNER JOIN lookup_zones pickup  
-    ON t.pickup_location_id  = pickup.location_id
-INNER JOIN lookup_zones dropoff 
-    ON t.dropoff_location_id = dropoff.location_id
-WHERE 
-    t.row_num = 1
+    ft.shared_ride_flag     as shared_ride_flag,
+    ft.pickup_datetime      as pickup_datetime,
+    ft.dropoff_datetime     as dropoff_datetime
+from 
+    fhv_trips ft
+inner join 
+    lookup_zones pickup on ft.pickup_location_id = pickup.location_id
+inner join 
+    lookup_zones dropoff on ft.dropoff_location_id = dropoff.location_id
