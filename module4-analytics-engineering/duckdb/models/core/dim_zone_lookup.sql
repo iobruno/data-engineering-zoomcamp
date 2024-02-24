f4249934bc82f75a5705e29b3871b8ff284d7381
Bruno Oliveira <bruno.ariev@gmail.com>
2024-02-24 19:02:03 -0300

    Updates core/dim_zone_lookup that depends on seed file

--- module4-analytics-engineering/duckdb/models/core/dim_zone_lookup.sql
+++ module4-analytics-engineering/duckdb/models/core/dim_zone_lookup.sql
@@ -1,7 +1,7 @@
-SELECT
-    location_id,
-    borough,
-    zone,
-    service_zone
-FROM
-    {{ ref('stg_zone_lookup') }}
+select
+    LocationID      as location_id,
+    Borough         as borough,
+    Zone            as zone,
+    service_zone    as service_zone
+from
+    {{ ref('taxi_zone_lookup') }}
