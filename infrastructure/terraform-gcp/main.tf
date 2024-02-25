b072a57b048fd3080d4acf02d876781e595790fb
Bruno Oliveira <bruno.ariev@gmail.com>
2024-02-25 04:49:06 -0300

    Modularizes bigquery into a tf module

--- infrastructure/terraform-gcp/main.tf
+++ infrastructure/terraform-gcp/main.tf
@@ -1,6 +1,8 @@
-resource "google_bigquery_dataset" "this" {
-  dataset_id = var.raw_nyc_tlc_record_dataset
-  location   = var.data_region
+module "bigquery" {
+  source = "./bigquery"
+
+  dataset_id  = var.raw_nyc_tlc_record_dataset
+  region      = var.data_region
 }
 
 module "gcs" {
