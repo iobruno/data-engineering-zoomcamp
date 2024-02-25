47c8ba55ad17d26b0bc903daab15ecc3a3a6c352
Bruno Oliveira <bruno.ariev@gmail.com>
2024-02-25 00:21:05 -0300

    Refactor staging models to use the new Glue Database

--- module4-analytics-engineering/redshift/models/staging/stg_green_tripdata.sql
+++ module4-analytics-engineering/redshift/models/staging/stg_green_tripdata.sql
@@ -2,6 +2,16 @@
     schema=resolve_schema_for('staging')
 ) }}
 
+with green_taxi_trips as (
+    select
+        row_number() over(partition by vendorid, lpep_pickup_datetime) as row_num,
+        gt.*
+    from
+        {{ source('raw_nyc_tlc_record_data', 'ext_green_taxi') }} gt
+    where
+        VendorID is not null        
+)
+
 select
     -- identifiers
     {{ dbt_utils.generate_surrogate_key([
@@ -33,10 +43,9 @@ select
     payment_type                          as payment_type,
     {{ payment_desc_of('payment_type') }} as payment_type_desc
 from 
-    {{ source('redshift-raw-nyc-trip_record', 'green') }}
+    green_taxi_trips
 where
-    VendorID is not null
-
+    row_num = 1
 
 -- Run as:
 --  dbt build --select stg_green_tripdata --vars 'is_test_run: true'
