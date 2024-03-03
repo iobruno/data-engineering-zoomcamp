769831463fc51795494f284984916c640075c2ac
Bruno Oliveira <bruno.ariev@gmail.com>
2024-03-02 22:21:57 -0300

    Fix indentation and minor bugs due to refactoring

--- module1-data-ingestion/python-ingest/run.py
+++ module1-data-ingestion/python-ingest/run.py
@@ -71,31 +71,29 @@ def extract_load_with(
 
     # This is required since, in 'append' mode, polars.df does not create
     # the table if it doesn't exist. It also guarantees idempotency
-    repo.save(df_slice, if_table_exists=write_disposition)
+    repo.save(df_slice, write_disposition=write_disposition)
     progress.update(task_id=tid, completed=completeness, total=total_parts)
 
     for _ in repo.save_all(other_slices):
         completeness += 1
         progress.update(task_id=tid, completed=completeness, total=total_parts)
 
-    extract_load_with(fetcher, repo, remain_endpoints, remain_tasks, if_table_exists="append")
+    extract_load_with(fetcher, repo, remain_endpoints, remain_tasks, write_disposition="append")
 
 
-# fmt: off
 @cli.command(name="ingest", help="CLI app to extract NYC Trips data and load into Postgres")
 def ingest_db(
     db_name: Annotated[str, Argument(envvar="DATABASE_NAME", hidden=True)],
-    db_username: Annotated[str, Argument(envvar="DATABASE_USERNAME", hidden=True,)],
-    db_password: Annotated[str, Argument(envvar="DATABASE_PASSWORD", hidden=True,)],
+    db_username: Annotated[str, Argument(envvar="DATABASE_USERNAME", hidden=True)],
+    db_password: Annotated[str, Argument(envvar="DATABASE_PASSWORD", hidden=True)],
     db_host: Annotated[str, Argument(envvar="DATABASE_HOST", hidden=True)],
     db_port: Annotated[str, Argument(envvar="DATABASE_PORT", hidden=True)] = 5432,
     yellow: Annotated[bool, Option("--yellow", "-y", help="Fetch Yellow taxi dataset")] = False,
     green: Annotated[bool, Option("--green", "-g", help="Fetch Green cab dataset")] = False,
     fhv: Annotated[bool, Option("--fhv", "-f", help="Fetch FHV cab dataset")] = False,
     zones: Annotated[bool, Option("--zones", "-z", help="Fetch Zone lookup dataset")] = False,
-    polars_ff: Annotated[bool, Option("--use-polars", help="Feature flag to enable Polars")] = False
+    polars_ff: Annotated[bool, Option("--use-polars", help="Feature flag to use Polars")] = False,
 ):
-    # fmt: on
     log.info("Connecting to 'postgres' with credentials on ENV VARs...")
     db_settings = db_host, db_port, db_name, db_username, db_password
     with progress:
@@ -117,7 +115,7 @@ def ingest_db(
         def gen_progress_tasks_for(endpoints: List[str]):
             filenames = [Path(endpoint).stem for endpoint in endpoints]
             return [
-                progress.add_task(name, start=False, total=float('inf'), completed=0)
+                progress.add_task(name, start=False, total=float("inf"), completed=0)
                 for name in filenames
             ]
 
@@ -127,6 +125,7 @@ def ingest_db(
             fetcher = df_fetcher\
                 .with_schema(schema_ref.green_taxi())\
                 .with_renaming_strategy(rename.green_taxi())
+
             extract_load_with(fetcher, green_repo, green_dataset_endpoints, green_tasks)
 
         if yellow and yellow_dataset_endpoints:
@@ -135,6 +134,7 @@ def ingest_db(
             fetcher = df_fetcher\
                 .with_schema(schema_ref.yellow_taxi())\
                 .with_renaming_strategy(rename.yellow_taxi())
+
             extract_load_with(fetcher, yellow_repo, yellow_dataset_endpoints, yellow_tasks)
 
         if fhv and fhv_dataset_endpoints:
@@ -143,6 +143,7 @@ def ingest_db(
             fetcher = df_fetcher\
                 .with_schema(schema_ref.fhv_taxi())\
                 .with_renaming_strategy(rename.fhv_taxi())
+
             extract_load_with(fetcher, fhv_repo, fhv_dataset_endpoints, fhv_tasks)
 
         if zones and zones_dataset_endpoints:
@@ -151,6 +152,7 @@ def ingest_db(
             fetcher = df_fetcher\
                 .with_schema(schema_ref.zone_lookup())\
                 .with_renaming_strategy(rename.zone_lookup())
+
             extract_load_with(fetcher, zone_repo, zones_dataset_endpoints, zone_tasks)
 
 
