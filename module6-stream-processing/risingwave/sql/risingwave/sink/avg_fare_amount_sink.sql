17206186b35f4f047e6c320410c81851dedecfcb
Bruno Oliveira <bruno.ariev@gmail.com>
2024-03-16 03:53:09 -0300

    Update SQL files
    * Drops sql/clickhouse/demo_table.sql, and sql/risingwave/sink/demo_clickhouse.sql
    
    * Updates table names on sql/clickhouse/avg_fare_amt.sql and sql/risingwave/sink/avg_fare_amount_sink.sql

--- module6-stream-processing/risingwave/sql/risingwave/sink/avg_fare_amount_sink.sql
+++ module6-stream-processing/risingwave/sql/risingwave/sink/avg_fare_amount_sink.sql
@@ -1,16 +1,16 @@
-create sink if not exists avg_fare_amt_sink as 
+create sink if not exists avg_fare_amout_sink as 
 select 
     avg_fare_amount_per_hour, 
     num_rides_per_hour 
 from 
-    avg_fare_amt
+    avg_fare_amount
 with (
     connector = 'clickhouse',
     type = 'append-only',
     clickhouse.url = 'http://clickhouse:8123',
-    clickhouse.user = '',
-    clickhouse.password = '',
+    clickhouse.user = 'clickhouse',
+    clickhouse.password = 'clickhouse',
     clickhouse.database = 'default',
-    clickhouse.table='avg_fare_amt',
+    clickhouse.table='avg_fare_amount',
     force_append_only = 'true'
 );
