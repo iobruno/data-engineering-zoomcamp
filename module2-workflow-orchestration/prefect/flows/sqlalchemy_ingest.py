d9cfb79c591313f726e3d2e9d846b124ee62b79a
Bruno Oliveira <bruno.ariev@gmail.com>
2024-07-10 20:53:46 -0300

    Update prefect flows for logging

--- module2-workflow-orchestration/prefect/flows/sqlalchemy_ingest.py
+++ module2-workflow-orchestration/prefect/flows/sqlalchemy_ingest.py
@@ -1,110 +1,94 @@
-import logging
 import math
-import os
+from os import getenv
 from pathlib import Path
-from typing import List
+from typing import List, Tuple
 
 import numpy as np
 import pandas as pd
-from omegaconf import DictConfig, OmegaConf
-from prefect_sqlalchemy import ConnectionComponents, SqlAlchemyConnector, SyncDriver
+from hydra import compose, initialize
+from prefect_sqlalchemy import SqlAlchemyConnector
 
-from prefect import flow, task
-
-config_file = Path(__file__).parent.parent.joinpath("app.yml")
-cfg = OmegaConf.load(config_file)
-
-logging.basicConfig(
-    format="%(asctime)s %(levelname)s %(name)s - %(message)s",
-    level=logging.INFO,
-    datefmt="%Y-%m-%dT%H:%M:%S",
-)
-
-log = logging.getLogger("flow_pg_ingest")
+from prefect import flow, get_run_logger, task
 
 
-def split_df_in_chunks_with(df, chunk_size: int = 1_000_000) -> (List[pd.DataFrame], int):
+def split_df_in_chunks_with(df, chunk_size: int = 1_000_000) -> Tuple[List[pd.DataFrame], int]:
     chunks_qty = math.ceil(len(df) / chunk_size)
     return np.array_split(df, chunks_qty), chunks_qty
 
 
-@task(log_prints=True, retries=3)
+@task(name="serialize-df-into-db", retries=3)
 def load_db_with(sqlalchemy: SqlAlchemyConnector, df: pd.DataFrame, tbl_name: str, label: str):
+    log = get_run_logger()
     chunks, qty = split_df_in_chunks_with(df)
     with sqlalchemy.get_connection(begin=False) as engine:
         for chunk_id, df_chunk in enumerate(chunks):
-            print(f"[{label}] Now saving: Chunk {chunk_id+1}/{qty}")
+            log.info(f"[{label}] Now saving: Chunk {chunk_id+1}/{qty}")
             df_chunk.to_sql(tbl_name, con=engine, if_exists="append", index=False)
 
 
-@task(log_prints=True)
+@task(name="drop-invalid-entries")
 def drop_trips_with_no_passengers(df: pd.DataFrame) -> pd.DataFrame:
+    log = get_run_logger()
     refined_df = df[df["passenger_count"] != 0]
     excluded_entries = df["passenger_count"].isin([0]).sum()
-    print(f"Excluded entries with zero passengers: {excluded_entries}")
+    log.info(f"Excluded entries with zero passengers: {excluded_entries}")
     return refined_df
 
 
-@task(log_prints=True, retries=3)
-def extract_nyc_trip_data_with(url: str) -> pd.DataFrame:
-    print(f"Now fetching: {url}")
+@task(name="fetch-dataset", retries=3)
+def fetch_csv_from(url: str) -> pd.DataFrame:
+    log = get_run_logger()
+    log.info(f"Now fetching: {url}")
     return pd.read_csv(url, engine="pyarrow")
 
 
-@task(log_prints=True)
-def prepare_sqlalchemy_block(sqlalchemy: DictConfig) -> SqlAlchemyConnector:
-    """
-    This attempts to load SqlAlchemy Prefect Block configured for Postgres
-    with the name defined in app.yml under the key prefect_block.sqlalchemy.ny_taxi.alias
-
-    If it fails to fetch such block, it will attempt to create one
-    using the 'database' defined in the app.yml under the same key.
-
-    As for the 'hostname', 'port', 'username' and 'password',
-    They'll be set from the env variables, respectively: DATABASE_HOST,
-    DATABASE_PORT, DATABASE_USERNAME and DATABASE_PASSWORD
-
-    :param sqlalchemy: DictConfig with keys for alias, database, hostname, and port
-    :return: SqlAlchemyConnector
-    """
+@task(name="prepare-sqlachemy-block")
+def prepare_sqlalchemy_block(sqlalchemy) -> SqlAlchemyConnector:
+    log = get_run_logger()
     try:
-        print(f"Attempting to load SqlAlchemy Block '{sqlalchemy.alias}'")
+        log.info(f"Attempting to load SqlAlchemy Block '{sqlalchemy.alias}'")
         conn_block = SqlAlchemyConnector.load(sqlalchemy.alias)
-        print(f"SqlAlchemy loaded successfully!")
+        log.info(f"SqlAlchemy loaded successfully!")
     except ValueError:
-        print(f"SqlAlchemy Block not found. Working on creating it...")
+        log.warning(f"SqlAlchemy Block not found. Working on creating it...")
+        user = getenv("DATABASE_USERNAME")
+        passwd = getenv("DATABASE_PASSWORD")
+        host = getenv("DATABASE_HOST")
+        port = getenv("DATABASE_PORT", 5432)
+        dbname = getenv("DATABASE_NAME")
         conn_block = SqlAlchemyConnector(
-            connection_info=ConnectionComponents(
-                driver=SyncDriver.POSTGRESQL_PSYCOPG2,
-                database=sqlalchemy.get("database"),
-                host=os.environ["DATABASE_HOST"],
-                port=os.environ["DATABASE_PORT"],
-                username=os.environ["DATABASE_USERNAME"],
-                password=os.environ["DATABASE_PASSWORD"],
-            )
+            connection_info=f"postgresql+psycopg://{user}:{passwd}@{host}:{port}/{dbname}"
         )
         conn_block.save(sqlalchemy.alias, overwrite=True)
 
     return conn_block
 
 
-@flow(name="NYC Trip CSV Dataset to Postgres", log_prints=True)
-def sqlalchemy_ingest():
-    print("Fetching URL Datasets from .yml")
-    datasets: DictConfig = cfg.datasets
-    sqlalchemy: DictConfig = cfg.prefect.sqlalchemy.nyc_taxi
+def load_conf():
+    with initialize(version_base=None, config_path="../", job_name="py-ingest"):
+        return compose(config_name="app")
 
-    print("Preparing Prefect Block...")
-    conn_block = prepare_sqlalchemy_block(sqlalchemy)
+
+@flow(name="web-csv-to-sqlalchemy")
+def sqlalchemy_ingest():
+    log = get_run_logger()
+    log.info("Fetching URL Datasets from .yaml")
+    cfg = load_conf()
+
+    prefect_cfg = cfg.prefect
+    datasets = cfg.datasets
+
+    log.info("Preparing Prefect Block...")
+    conn_block = prepare_sqlalchemy_block(prefect_cfg.sqlalchemy)
 
     for dataset, endpoints in datasets.items():
         if endpoints is None:
-            print(f"Dataset '{dataset}' contains no valid endpoints entries. Skipping...")
+            log.info(f"Dataset '{dataset}' contains no valid endpoints entries. Skipping...")
             endpoints = []
 
         for endpoint in endpoints:
             filename = Path(endpoint).name
-            df = extract_nyc_trip_data_with(url=endpoint)
+            df = fetch_csv_from(url=endpoint)
 
             if dataset in {"yellow", "green"}:
                 persisting_df = drop_trips_with_no_passengers(df)
@@ -117,6 +101,7 @@ def sqlalchemy_ingest():
                 tbl_name=dataset,
                 label=filename,
             )
+
 
 if __name__ == "__main__":
     sqlalchemy_ingest()
