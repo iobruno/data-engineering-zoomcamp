9cb34dbc7076d532e96687425f31843b6efe8b0a
Bruno Oliveira <bruno.ariev@gmail.com>
2024-07-10 15:08:57 -0300

    Refactor df_repository to support psycopg3

--- module1-data-ingestion/python-ingest/src/dataframe_repository.py
+++ module1-data-ingestion/python-ingest/src/dataframe_repository.py
@@ -1,17 +1,18 @@
 from abc import ABCMeta, abstractmethod
-from typing import List, Optional
+from typing import List, Literal, Optional
 
 import pandas as pd
 import polars as pl
-import sqlalchemy
 
 
 class SQLRepository(metaclass=ABCMeta):
-    def __init__(self, conn_string: str):
+    def __init__(self, conn_string: str, adbc_conn_string):
         self.conn_string = conn_string
-        self.conn = sqlalchemy.create_engine(conn_string).connect()
+        self.adbc_conn_string = adbc_conn_string
 
-    def save(self, df: pd.DataFrame | pl.DataFrame, write_disposition: str) -> Optional[int]:
+    def save(
+        self, df: pd.DataFrame | pl.DataFrame, write_disposition: Literal["replace", "append"]
+    ) -> Optional[int]:
         if isinstance(df, pl.DataFrame):
             return self.save_polars_df(df, write_disposition)
         elif isinstance(df, pd.DataFrame):
@@ -20,16 +21,23 @@ class SQLRepository(metaclass=ABCMeta):
             "Unsupported Dataframe type." "Supported types are pandas or polars Dataframes only"
         )
 
-    def save_polars_df(self, df: pl.DataFrame, write_disposition: str, engine="adbc"):
+    def save_polars_df(
+        self,
+        df: pl.DataFrame,
+        write_disposition: Literal["replace", "append"],
+        engine: Literal["adbc", "sqlalchemy"] = "adbc",
+    ):
         return df.write_database(
             table_name=self.tbl_name,
-            connection=self.conn_string,
-            if_table_exists=write_disposition,
             engine=engine,
+            connection=self.adbc_conn_string if engine == "adbc" else self.conn_string,
+            if_table_exists=write_disposition,
         )
 
     def save_pandas_df(self, df: pd.DataFrame, write_disposition):
-        return df.to_sql(self.tbl_name, con=self.conn, if_exists=write_disposition, index=False)
+        return df.to_sql(
+            self.tbl_name, con=self.conn_string, if_exists=write_disposition, index=False
+        )
 
     def save_all(self, chunks: List[pd.DataFrame]):
         for chunk in chunks:
@@ -42,10 +50,11 @@ class SQLRepository(metaclass=ABCMeta):
 
     @classmethod
     def with_config(cls, *db_settings) -> "SQLRepository":
-        (db_host, db_port, db_name, db_username, db_password) = db_settings
+        (db_host, db_port, db_name, db_user, db_passwd) = db_settings
         db_port = 5432 if db_port is None else db_port
-        conn_string = f"postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
-        return cls.__call__(conn_string=conn_string)
+        conn_string = f"postgresql+psycopg://{db_user}:{db_passwd}@{db_host}:{db_port}/{db_name}"
+        adbc_conn_string = f"postgresql://{db_user}:{db_passwd}@{db_host}:{db_port}/{db_name}"
+        return cls.__call__(conn_string=conn_string, adbc_conn_string=adbc_conn_string)
 
 
 class GreenTaxiRepository(SQLRepository):
