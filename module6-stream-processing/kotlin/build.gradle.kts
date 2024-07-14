09d9ebb6e001773165ce8392a53809d19fcc441a
Bruno Oliveira <bruno.ariev@gmail.com>
2024-07-14 15:56:56 -0300

    Update dependencies and runtimes
    * Updates Kotlin runtime to 2.0.0
    * Updates all project dependency libraries
    * Bumps gradle to 8.9

--- module6-stream-processing/kotlin/build.gradle.kts
+++ module6-stream-processing/kotlin/build.gradle.kts
@@ -2,8 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
-    kotlin("jvm") version "1.8.10"
-    id("com.github.johnrengelman.shadow") version "8.1.0"
+    kotlin("jvm") version "2.0.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 val artifactName = "kafka-stream-processing"
@@ -12,34 +12,25 @@ val artifactVersion = "1.0-SNAPSHOT"
 repositories {
     mavenLocal()
     mavenCentral()
-    maven {
-        name = "Confluent Maven Repository"
-        url = uri("https://packages.confluent.io/maven/")
-    }
 }
 
 dependencies {
-    val confluentKafkaVersion = "7.3.2"
-    val avroVersion = "1.11.1"
-    val protobufVersion = "3.22.0"
-    val jacksonVersion = "2.14.2"
-    val picocliVersion = "4.7.1"
+    val confluentKafkaVersion = "7.6.1"
+    val protobufVersion = "4.27.2"
+    val avroVersion = "1.11.3"
+    val jacksonVersion = "2.17.2"
+    val picocliVersion = "4.7.6"
 
-    val kotlinLoggingVersion = "3.0.5"
-    val logbackVersion = "1.4.5"
+    val kotlinLoggingVersion = "7.0.0"
+    val logbackVersion = "1.5.6"
 
-    val junitVersion = "5.9.2"
-    val mockKVersion = "1.13.4"
-    val kotestVersion = "5.5.5"
+    val kotestVersion = "5.9.1"
+    val mockKVersion = "1.13.11"
+    val junitVersion = "5.10.3"
 
     implementation("org.apache.kafka:kafka-clients:${confluentKafkaVersion}-ccs")
     implementation("org.apache.kafka:kafka-streams:${confluentKafkaVersion}-ccs")
     implementation("io.confluent:kafka-schema-registry-client:${confluentKafkaVersion}")
-
-    /** Kafka and Kafka Streams Serde for Avro **/
-    implementation("io.confluent:kafka-avro-serializer:${confluentKafkaVersion}")
-    implementation("io.confluent:kafka-streams-avro-serde:${confluentKafkaVersion}")
-    implementation("org.apache.avro:avro:${avroVersion}")
 
     /** Kafka and Kafka Streams Serde for Protobuf **/
     implementation("io.confluent:kafka-protobuf-serializer:${confluentKafkaVersion}")
@@ -50,6 +41,11 @@ dependencies {
     implementation("io.confluent:kafka-json-serializer:${confluentKafkaVersion}")
     implementation("io.confluent:kafka-streams-json-schema-serde:${confluentKafkaVersion}")
 
+    /** Kafka and Kafka Streams Serde for Avro **/
+    implementation("io.confluent:kafka-avro-serializer:${confluentKafkaVersion}")
+    implementation("io.confluent:kafka-streams-avro-serde:${confluentKafkaVersion}")
+    implementation("org.apache.avro:avro:${avroVersion}")
+
     /** CLI Builder **/
     implementation("info.picocli:picocli:$picocliVersion")
 
@@ -59,14 +55,14 @@ dependencies {
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${jacksonVersion}")
 
     /** Logging **/
-    implementation("io.github.microutils:kotlin-logging-jvm:${kotlinLoggingVersion}")
+    implementation("io.github.oshai:kotlin-logging-jvm:${kotlinLoggingVersion}")
     implementation("ch.qos.logback:logback-classic:${logbackVersion}")
 
     /** Unit and Integration Test Frameworks **/
     testImplementation("org.apache.kafka:kafka-streams-test-utils:${confluentKafkaVersion}-ccs")
-    testImplementation("org.junit.jupiter:junit-jupiter:$junitVersion")
-    testImplementation("io.mockk:mockk:$mockKVersion")
     testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
+    testImplementation("io.mockk:mockk:$mockKVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter:$junitVersion")
 }
 
 tasks.withType<ShadowJar> {
