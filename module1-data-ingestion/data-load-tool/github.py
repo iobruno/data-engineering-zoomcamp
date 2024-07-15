9f2e1e096858bfa2c9029c1face7ac0c6047a9d5
Bruno Oliveira <bruno.ariev@gmail.com>
2024-07-15 19:15:38 -0300

    refactor github module with dlt.source refactor download_links_group_by

--- module1-data-ingestion/data-load-tool/github.py
+++ module1-data-ingestion/data-load-tool/github.py
@@ -1,23 +1,34 @@
+import operator as op
+from functools import reduce
+from typing import Any, Dict
+
 import dlt
 import httpx
-from typing import List, Dict
-from itertools import groupby
+from pipe import groupby, map, select, tee
 
 releases_endpoint = "https://api.github.com/repos/{owner}/{repo}/releases"
 
 
-@dlt.resource
-def fetch_releases_from(owner: str, repo: str) -> List[Dict]:
-    endpoint = releases_endpoint.format(owner=owner, repo=repo)
-    response = httpx.get(endpoint)
-    response.raise_for_status()
-    yield response.json()
+@dlt.source
+def releases(owner: str, repo: str):
 
+    @dlt.resource
+    def fetch_all():
+        endpoint = releases_endpoint.format(owner=owner, repo=repo)
+        response = httpx.get(endpoint)
+        response.raise_for_status()
+        yield response.json()
 
-@dlt.resource
-def fetch_releases_from_grouping_by(owner: str, repo: str, group_key: str) -> Dict:
-    raw_json = fetch_releases_from(owner=owner, repo=repo)
-    yield {
-        group_key: list(artifacts)[0]
-        for group_key, artifacts in groupby(raw_json, lambda key: key[group_key])
-    }
+    @dlt.transformer(data_from=fetch_all)
+    def download_links_group_by(raw_json: Any, key: str) -> Dict:
+        yield (
+            raw_json
+            | groupby(lambda grouping: grouping[key])
+            | map(lambda g: {g[0]: list(g[1])[0].get('assets')})
+            | map(lambda kv: {k: parse_download_link(v) for k, v in kv.items()})
+        )
+
+    def parse_download_link(mapping):
+        return list(mapping | select(lambda item: item.get('browser_download_url')))
+
+    return [fetch_all, download_links_group_by]
