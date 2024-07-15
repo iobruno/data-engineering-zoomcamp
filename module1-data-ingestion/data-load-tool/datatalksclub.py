619576bc25cd85f50c60350d4ffdad7107e0458b
Bruno Oliveira <bruno.ariev@gmail.com>
2024-07-15 02:18:13 -0300

    Add dlt resources for datatalksclub datasets for * green_taxi_trip_data * yellow_taxi_trip_data * fhv_trip_data * zone_lookup

---
+++ module1-data-ingestion/data-load-tool/datatalksclub.py
@@ -0,0 +1,97 @@
+import dlt
+import polars as pl
+from typing import List, Generator
+
+
+def fetch_all(endpoints: List[str], schema=None) -> Generator:
+    for endpoint in endpoints:
+        yield pl.read_csv(endpoint, schema_overrides=schema).to_arrow()
+
+
+@dlt.resource(name="green_trip_data")
+def green_trip_data(endpoints: List[str]):
+    yield fetch_all(endpoints=endpoints, schema=_green_trip_data_schema())
+
+
+@dlt.resource(name="yellow_trip_data")
+def yellow_trip_data(endpoints: List[str]):
+    yield fetch_all(endpoints=endpoints, schema=_yellow_trip_data_schema())
+
+
+@dlt.resource(name="fhv_trip_data")
+def fhv_trip_data(endpoints: List[str]):
+    yield fetch_all(endpoints=endpoints, schema=_fhv_trip_data_schema())
+
+
+@dlt.resource(name="zone_lookup")
+def zone_lookup_data(endpoints: List[str]):
+    yield fetch_all(endpoints=endpoints, schema=_zone_lookups_schema())
+
+
+def _green_trip_data_schema():
+    return {
+        "VendorID": pl.Int32,
+        "lpep_pickup_datetime": pl.Datetime,
+        "lpep_dropoff_datetime": pl.Datetime,
+        "passenger_count": pl.Int8,
+        "trip_distance": pl.Float64,
+        "PULocationID": pl.Int32,
+        "DOLocationID": pl.Int32,
+        "RatecodeID": pl.Int8,
+        "store_and_fwd_flag": pl.String,
+        "payment_type": pl.Int8,
+        "fare_amount": pl.Float64,
+        "extra": pl.Float64,
+        "mta_tax": pl.Float64,
+        "improvement_surcharge": pl.Float64,
+        "tip_amount": pl.Float64,
+        "tolls_amount": pl.Float64,
+        "total_amount": pl.Float64,
+        "congestion_surcharge": pl.Float64,
+        "ehail_fee": pl.Float64,
+        "trip_type": pl.Int8,
+    }
+
+
+def _yellow_trip_data_schema():
+    return {
+        "VendorID": pl.Int32,
+        "tpep_pickup_datetime": pl.Datetime,
+        "tpep_dropoff_datetime": pl.Datetime,
+        "passenger_count": pl.Int8,
+        "trip_distance": pl.Float64,
+        "PULocationID": pl.Int32,
+        "DOLocationID": pl.Int32,
+        "RatecodeID": pl.Int8,
+        "store_and_fwd_flag": pl.String,
+        "payment_type": pl.Int8,
+        "fare_amount": pl.Float64,
+        "extra": pl.Float64,
+        "mta_tax": pl.Float64,
+        "improvement_surcharge": pl.Float64,
+        "tip_amount": pl.Float64,
+        "tolls_amount": pl.Float64,
+        "total_amount": pl.Float64,
+        "congestion_surcharge": pl.Float64,
+    }
+
+
+def _fhv_trip_data_schema():
+    return {
+        "dispatching_base_num": pl.String,
+        "pickup_datetime": pl.String,
+        "dropOff_datetime": pl.String,
+        "PUlocationID": pl.Int32,
+        "DOlocationID": pl.Int32,
+        "SR_Flag": pl.String,
+        "Affiliated_base_number": pl.String,
+    }
+
+
+def _zone_lookups_schema():
+    return {
+        "LocationID": pl.Int32,
+        "Borough": pl.String,
+        "Zone": pl.String,
+        "service_zone": pl.String,
+    }
