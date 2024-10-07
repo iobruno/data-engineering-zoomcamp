1851e58149ac2bdb1d632e12ea3867c31abc8be5
Bruno Oliveira <bruno.ariev@gmail.com>
2024-10-07 00:09:55 -0300

    chore/dbt-clickhouse-update (#120)
    * Update project dependencies
    * Update migrations to enable Federated Queries on Postgres/MySQL
    * Update Dockerfile accordingly
    * Updates running instructions on README
    * Updates postgres and clickhouse infrastructure on compose.yaml
        * Bumps postgres to 17-alpine
        * Bumps clickhouse to 24.9.2-alpine

--- module4-analytics-engineering/clickhouse/migrations/postgres_engine.sql
+++ module4-analytics-engineering/clickhouse/migrations/postgres_engine.sql
@@ -1,2 +1,2 @@
 CREATE DATABASE fqdb_nyc_taxi
-ENGINE = PostgreSQL('postgres:5432', 'nyc_taxi', 'postgres', 'postgres', 'public', 0);
+ENGINE = PostgreSQL('host.docker.internal:5432', 'nyc_taxi', 'postgres', 'postgres', 'public', 0);
