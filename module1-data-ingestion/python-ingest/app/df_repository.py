8604683b1fcec5462efb5fa191e0685c306196d9
iobruno <bruno.ariev@gmail.com>
2024-12-09 20:33:48 -0300

    Refactor df_repository * Drops with_config class method * Update package name (from src/ to app/) * Reformats module with ruff format and lint

--- module1-data-ingestion/python-ingest/app/df_repository.py
+++ module1-data-ingestion/python-ingest/app/df_repository.py
@@ -6,20 +6,19 @@ import polars as pl
 
 
 class SQLRepo(metaclass=ABCMeta):
-    def __init__(self, conn_string: str, adbc_conn_string):
+    def __init__(self, conn_string: str):
         self.conn_string = conn_string
-        self.adbc_conn_string = adbc_conn_string
 
     def save(
-        self, df: pd.DataFrame | pl.DataFrame, write_disposition: Literal["replace", "append"]
+        self,
+        df: pd.DataFrame | pl.DataFrame,
+        write_disposition: Literal["replace", "append"],
     ) -> Optional[int]:
         if isinstance(df, pl.DataFrame):
             return self.save_polars_df(df, write_disposition)
         elif isinstance(df, pd.DataFrame):
             return self.save_pandas_df(df, write_disposition)
-        raise RuntimeError(
-            "Unsupported Dataframe type." "Supported types are pandas or polars Dataframes only"
-        )
+        raise RuntimeError("Unsupported DataFrame type. Only pandas or polars are supported")
 
     def save_polars_df(
         self,
@@ -30,13 +29,16 @@ class SQLRepo(metaclass=ABCMeta):
         return df.write_database(
             table_name=self.tbl_name,
             engine=engine,
-            connection=self.adbc_conn_string if engine == "adbc" else self.conn_string,
+            connection=self.conn_string,
             if_table_exists=write_disposition,
         )
 
     def save_pandas_df(self, df: pd.DataFrame, write_disposition):
         return df.to_sql(
-            self.tbl_name, con=self.conn_string, if_exists=write_disposition, index=False
+            self.tbl_name,
+            con=self.conn_string,
+            if_exists=write_disposition,
+            index=False,
         )
 
     def save_all(self, chunks: List[pd.DataFrame]):
@@ -47,14 +49,6 @@ class SQLRepo(metaclass=ABCMeta):
     @abstractmethod
     def tbl_name(self) -> str:
         raise NotImplementedError()
-
-    @classmethod
-    def with_config(cls, *db_settings) -> "SQLRepo":
-        (db_host, db_port, db_name, db_user, db_passwd) = db_settings
-        db_port = 5432 if db_port is None else db_port
-        conn_string = f"postgresql+psycopg://{db_user}:{db_passwd}@{db_host}:{db_port}/{db_name}"
-        adbc_conn_string = f"postgresql://{db_user}:{db_passwd}@{db_host}:{db_port}/{db_name}"
-        return cls.__call__(conn_string=conn_string, adbc_conn_string=adbc_conn_string)
 
 
 class GreenTaxiRepo(SQLRepo):
