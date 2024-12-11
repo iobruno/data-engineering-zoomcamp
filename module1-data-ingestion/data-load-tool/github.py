6511f48106686ecb05c1677b9c91aa20e1a4a9e7
iobruno <bruno.ariev@gmail.com>
2024-12-10 23:12:56 -0300

    Update typehints

--- module1-data-ingestion/data-load-tool/github.py
+++ module1-data-ingestion/data-load-tool/github.py
@@ -1,17 +1,12 @@
-import operator as op
-from functools import reduce
-from typing import Any, Dict
-
 import dlt
 import httpx
-from pipe import groupby, map, select, tee
+from pipe import groupby, map, select
 
 releases_endpoint = "https://api.github.com/repos/{owner}/{repo}/releases"
 
 
 @dlt.source
 def releases(owner: str, repo: str):
-
     @dlt.resource
     def fetch_all():
         endpoint = releases_endpoint.format(owner=owner, repo=repo)
@@ -20,15 +15,15 @@ def releases(owner: str, repo: str):
         yield response.json()
 
     @dlt.transformer(data_from=fetch_all)
-    def download_links_group_by(raw_json: Any, key: str) -> Dict:
+    def download_links_group_by(raw_json, key) -> Dict:
         yield (
             raw_json
             | groupby(lambda grouping: grouping[key])
-            | map(lambda g: {g[0]: list(g[1])[0].get('assets')})
+            | map(lambda g: {g[0]: list(g[1])[0].get("assets")})
             | map(lambda kv: {k: parse_download_link(v) for k, v in kv.items()})
         )
 
     def parse_download_link(mapping):
-        return list(mapping | select(lambda item: item.get('browser_download_url')))
+        return list(mapping | select(lambda item: item.get("browser_download_url")))
 
     return [fetch_all, download_links_group_by]
