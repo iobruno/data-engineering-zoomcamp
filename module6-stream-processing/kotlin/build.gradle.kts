dfd1c25c29416d25bd56d98a66274beae8a47ab4
iobruno <bruno.ariev@gmail.com>
2024-12-14 13:10:03 -0300

    Externalizes all dependency and plugin versions to gradle.properties

--- module6-stream-processing/kotlin/build.gradle.kts
+++ module6-stream-processing/kotlin/build.gradle.kts
@@ -1,16 +1,14 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 import io.gitlab.arturbosch.detekt.Detekt
 
 plugins {
-    kotlin("jvm") version "2.0.0"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("io.gitlab.arturbosch.detekt") version("1.23.6")
+    kotlin("jvm")
+    id("io.gitlab.arturbosch.detekt")
+    id("com.github.johnrengelman.shadow")
 }
 
-val artifactName = "kafka-stream-processing"
-val artifactVersion = "1.0-SNAPSHOT"
+val artifactName = "kotlin-sp"
+val artifactVersion = "2.0-SNAPSHOT"
 
 repositories {
     mavenLocal()
@@ -22,27 +20,28 @@ repositories {
 }
 
 dependencies {
-    val confluentKafkaVersion = "7.6.1"
-    val protobufVersion = "4.27.2"
-    val avroVersion = "1.11.3"
-    val jacksonVersion = "2.17.2"
-    val picocliVersion = "4.7.6"
-
-    val kotlinLoggingVersion = "7.0.0"
-    val logbackVersion = "1.5.6"
-
-    val kotestVersion = "5.9.1"
-    val mockKVersion = "1.13.11"
-    val junitVersion = "5.10.3"
+    val confluentKafkaVersion: String by project
+    val protobufVersion: String by project
+    val avroVersion: String by project
+    val jacksonVersion: String by project
+    val picocliVersion: String by project
+    val kotlinLoggingVersion: String by project
+    val logbackVersion: String by project
+    val kotestVersion: String by project
+    val mockKVersion: String by project
 
     implementation("org.apache.kafka:kafka-clients:${confluentKafkaVersion}-ccs")
     implementation("org.apache.kafka:kafka-streams:${confluentKafkaVersion}-ccs")
     implementation("io.confluent:kafka-schema-registry-client:${confluentKafkaVersion}")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${jacksonVersion}")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${jacksonVersion}")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv:${jacksonVersion}")
+    implementation("info.picocli:picocli:$picocliVersion")
 
     /** Kafka and Kafka Streams Serde for Protobuf **/
     implementation("io.confluent:kafka-protobuf-serializer:${confluentKafkaVersion}")
     implementation("io.confluent:kafka-streams-protobuf-serde:${confluentKafkaVersion}")
-    implementation("com.google.protobuf:protobuf-java:$protobufVersion")
+    implementation("com.google.protobuf:protobuf-kotlin:$protobufVersion")
 
     /** Kafka and Kafka Streams Serde for JSON Schema **/
     implementation("io.confluent:kafka-json-serializer:${confluentKafkaVersion}")
@@ -53,29 +52,28 @@ dependencies {
     implementation("io.confluent:kafka-streams-avro-serde:${confluentKafkaVersion}")
     implementation("org.apache.avro:avro:${avroVersion}")
 
-    /** CLI Builder **/
-    implementation("info.picocli:picocli:$picocliVersion")
-
-    /** CSV Parser **/
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-csv:${jacksonVersion}")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:${jacksonVersion}")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${jacksonVersion}")
-
     /** Logging **/
     implementation("io.github.oshai:kotlin-logging-jvm:${kotlinLoggingVersion}")
     implementation("ch.qos.logback:logback-classic:${logbackVersion}")
 
     /** Unit and Integration Test Frameworks **/
     testImplementation("org.apache.kafka:kafka-streams-test-utils:${confluentKafkaVersion}-ccs")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
     testImplementation("io.mockk:mockk:$mockKVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter:$junitVersion")
+}
+
+kotlin {
+    jvmToolchain(21)
 }
 
 detekt {
-    toolVersion = "1.23.6"
     config.setFrom(file("detekt.yml"))
     buildUponDefaultConfig = true
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
 
 tasks.withType<Detekt>().configureEach {
@@ -88,13 +86,6 @@ tasks.withType<Detekt>().configureEach {
     }
 }
 
-tasks.withType<KotlinJvmCompile>().configureEach {
-    compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_21)
-        freeCompilerArgs.add("-opt-in=kotlin.RequiresOptIn")
-    }
-}
-
 tasks.withType<ShadowJar> {
     manifest {
         attributes(mapOf("Main-Class" to "club.datatalks.kafka.CliApplication"))
@@ -104,8 +95,4 @@ tasks.withType<ShadowJar> {
     archiveClassifier.set("")
     isZip64 = true
     mergeServiceFiles()
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
 }
--- module6-stream-processing/kotlin/settings.gradle.kts
+++ module6-stream-processing/kotlin/settings.gradle.kts
@@ -1,1 +1,14 @@
-rootProject.name = "kafka-stream-processing"
+pluginManagement {
+    val kotlinVersion: String by settings
+    val detektVersion: String by settings
+    val shadowJarVersion: String by settings
+
+    plugins {
+        kotlin("jvm") version kotlinVersion
+        id("io.gitlab.arturbosch.detekt") version detektVersion
+        id("com.github.johnrengelman.shadow") version shadowJarVersion
+    }
+
+}
+
+rootProject.name = "kotlin-sp"
